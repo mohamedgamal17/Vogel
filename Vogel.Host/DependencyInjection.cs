5c90902d9c5ca0f91f93e1d1f278837fadee8482
Mohamed Gamal <mohamedsherf17@gmail.com>
2024-02-28 23:28:37 +0200

    Add Vogel Host

---
+++ Vogel.Host/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Hellang.Middleware.ProblemDetails;
+using Hellang.Middleware.ProblemDetails.Mvc;
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddVogelWeb(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddProblemDetails(opt =>
+            {
+                opt.MapToStatusCode<Exception>(StatusCodes.Status500InternalServerError);
+            }).AddControllers()
+            .AddProblemDetailsConventions();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
---
+++ Vogel.Host/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddVogelWeb(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
