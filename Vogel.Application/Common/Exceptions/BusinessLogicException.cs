8797aaaf0614cca14015ba67e7b810837e64e782
Mohamed Gamal <mohamedsherf17@gmail.com>
2024-03-17 21:22:11 +0200

    Add BusinessLogicException

---
+++ Vogel.Application/Common/Exceptions/BusinessLogicException.cs
@@ -0,0 +1,10 @@
+﻿namespace Vogel.Application.Common.Exceptions
+{
+    public class BusinessLogicException : Exception
+    {
+        public BusinessLogicException(string message) : base(message)
+        {
+
+        }
+    }
+}
