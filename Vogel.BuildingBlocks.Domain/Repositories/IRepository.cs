0331c442fc83bd665b4c03e40a7def63f73e2e1a
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-05-21 22:57:19 +0300

    Add IRepository Interface

---
+++ Vogel.BuildingBlocks.Domain/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Vogel.BuildingBlocks.Domain.Repositories
+{
+    public interface IRepository <TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> AsQuerable();
+        Task DeleteAsync(TEntity entity);
+        Task<TEntity> InsertAsync(TEntity entity);
+        Task<List<TEntity>> InsertManyAsync(List<TEntity> entities);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity?> FindByIdAsync(object id);
+        Task<TEntity?> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> expression);
+        Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> expression);
+    }
+}
