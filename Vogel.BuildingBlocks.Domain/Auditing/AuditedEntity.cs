ec90ba316ee9a66c6c02b7ec2536e8a614bb3098
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-05-23 02:52:07 +0300

    Refactor Auditting Entities

--- Vogel.BuildingBlocks.Domain/Auditing/AuditedEntity.cs
+++ Vogel.BuildingBlocks.Domain/Auditing/AuditedEntity.cs
@@ -4,10 +4,10 @@ namespace Vogel.BuildingBlocks.Domain.Auditing
     public class AuditedEntity<TKey> : Entity<TKey>, IAuditedEntity
     {
         public string? CreatorId { get ; set ; }
-        public DateTime CreationTime { get ; set ; }
-        public DateTime? ModificationTime { get ; set ; }
+        public DateTimeOffset CreationTime { get ; set ; }
+        public DateTimeOffset? ModificationTime { get ; set ; }
         public string? ModifierId { get ; set ; }
-        public DateTime? DeletionTime { get ; set ; }
+        public DateTimeOffset? DeletionTime { get ; set ; }
         public string? DeletorId { get; set ; }
     }
 }
--- Vogel.BuildingBlocks.Domain/Auditing/IAuditedEntity.cs
+++ Vogel.BuildingBlocks.Domain/Auditing/IAuditedEntity.cs
@@ -3,10 +3,10 @@
     public interface IAuditedEntity
     {
         public string? CreatorId { get; set; }
-        public DateTime CreationTime { get; set; }
-        public DateTime? ModificationTime { get; set; }
+        public DateTimeOffset CreationTime { get; set; }
+        public DateTimeOffset? ModificationTime { get; set; }
         public string? ModifierId { get; set; }
-        public DateTime? DeletionTime { get; set; }
+        public DateTimeOffset? DeletionTime { get; set; }
         public string? DeletorId { get; set; }
     }
 }
