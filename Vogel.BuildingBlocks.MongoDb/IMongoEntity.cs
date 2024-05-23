d125890d4d86e99d0622c7ff608fcc036d9965cf
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-05-24 00:13:18 +0300

    Add Base Mongo Db Auditing Entity

--- Vogel.BuildingBlocks.MongoDb/IMongoEntity.cs
+++ Vogel.BuildingBlocks.MongoDb/IMongoEntity.cs
@@ -14,6 +14,4 @@
         public TKey Id { get; set; }
     }
 
-
-
 }
---
+++ Vogel.BuildingBlocks.MongoDb/MongoAuditing.cs
@@ -0,0 +1,23 @@
+﻿namespace Vogel.BuildingBlocks.MongoDb
+{
+    public interface IMongoAuditing
+    {
+         string? CreatorId { get; set; }
+         DateTimeOffset CreationTime { get; set; }
+         DateTimeOffset? ModificationTime { get; set; }
+         string? ModifierId { get; set; }
+         DateTimeOffset? DeletionTime { get; set; }
+         string? DeletorId { get; set; }
+    }
+
+    public class FullAuditedMongoEntity<TKey> :MongoEntity<TKey> , IMongoAuditing
+    {
+        public string? CreatorId { get; set; }
+        public DateTimeOffset CreationTime { get; set; }
+        public DateTimeOffset? ModificationTime { get; set; }
+        public string? ModifierId { get; set; }
+        public DateTimeOffset? DeletionTime { get; set; }
+        public string? DeletorId { get; set; }
+
+    }
+}
