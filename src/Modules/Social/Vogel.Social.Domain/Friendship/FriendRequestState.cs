a44f21455023f06dc75897b81b3ddbefc46520f5
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-12 02:41:46 +0300

    refactor : add social domain friendship entities

---
+++ src/Modules/Social/Vogel.Social.Domain/Friendship/FriendRequestState.cs
@@ -0,0 +1,10 @@
+﻿namespace Vogel.Social.Domain.Friendship
+{
+    public enum FriendRequestState
+    {
+        Pending = 0,
+        Accepted = 5,
+        Rejected = 10,
+        Cancelled = 15
+    }
+}
---
+++ src/Modules/Social/Vogel.Social.Domain/Friendship/FriendRequestTableConsts.cs
@@ -0,0 +1,13 @@
+﻿namespace Vogel.Social.Domain.Friendship
+{
+    public static class FriendRequestTableConsts
+    {
+        public static int IdLength = 256;
+
+        public const string TableName = "FriendRequests";
+
+        public static int SenderIdLenght = 256;
+
+        public static int ReciverIdLenght = 256;
+    }
+}
