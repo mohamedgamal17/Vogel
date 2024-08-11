5a1189eab37ec16bcaec3dadcf1edc45b9fe8fa5
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-12 02:47:38 +0300

    refector! : move paging model to mongodb lib

---
+++ src/Libraries/Vogel.BuildingBlocks.MongoDb/Models/Paging.cs
@@ -0,0 +1,8 @@
+﻿namespace Vogel.BuildingBlocks.MongoDb.Models
+{
+    public class Paging<T>
+    {
+        public List<T> Data { get; set; }
+        public PagingInfo Info { get; set; }
+    }
+}
---
+++ src/Libraries/Vogel.BuildingBlocks.MongoDb/Models/PagingInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace Vogel.BuildingBlocks.MongoDb.Models
+{
+    public class PagingInfo
+    {
+        public string? NextCursor { get; set; }
+        public string? PreviousCursor { get; set; }
+        public bool HasNext => NextCursor != null;
+        public bool HasPrevious => PreviousCursor != null;
+        public bool Ascending { get; set; }
+        public PagingInfo(string? nextCursor, string? previousCursor, bool ascending)
+        {
+            NextCursor = nextCursor;
+            PreviousCursor = previousCursor;
+            Ascending = ascending;
+        }
+    }
+}
