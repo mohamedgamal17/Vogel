5dec01d7cde7f7ff27573f6bcbc38330b5da9463
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-19 20:21:34 +0300

    refactor : add mongo picture  synchronization handler

---
+++ Vogel.Social.MongoEntities/Pictures/PictureMongoRepository.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+using Vogel.BuildingBlocks.MongoDb;
+
+namespace Vogel.Social.MongoEntities.Pictures
+{
+    public class PictureMongoRepository : MongoRepository<PictureMongoEntity>
+    {
+        public PictureMongoRepository(IMongoDatabase mongoDatabase) : base(mongoDatabase)
+        {
+        }
+    }
+}
---
+++ src/Modules/Social/Vogel.Social.Application/Pictures/Handlers/MongoSynchronizationHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using Vogel.BuildingBlocks.Domain.Events;
+using Vogel.Social.Domain.Pictures;
+using Vogel.Social.MongoEntities.Pictures;
+
+namespace Vogel.Social.Application.Pictures.Handlers
+{
+    internal class MongoSynchronizationHandler :
+        INotificationHandler<EntityCreatedEvent<Picture>>,
+        INotificationHandler<EntityUpdatedEvent<Picture>>,
+        INotificationHandler<EntityDeletedEvent<Picture>>
+    {
+        private readonly IMapper _mapper;
+
+        private readonly PictureMongoRepository _pictureMongoRepository;
+
+        public MongoSynchronizationHandler(IMapper mapper, PictureMongoRepository pictureMongoRepository)
+        {
+            _mapper = mapper;
+            _pictureMongoRepository = pictureMongoRepository;
+        }
+
+        public async Task Handle(EntityCreatedEvent<Picture> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Picture, PictureMongoEntity>(notification.Entity);
+
+            await _pictureMongoRepository.InsertAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityUpdatedEvent<Picture> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Picture, PictureMongoEntity>(notification.Entity);
+
+            await _pictureMongoRepository.UpdateAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityDeletedEvent<Picture> notification, CancellationToken cancellationToken)
+        {
+            await _pictureMongoRepository.DeleteAsync(notification.Entity.Id);
+        }
+    }
+}
