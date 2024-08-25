b08d79c566104450b04d2ad874c48802cbd9a9a9
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-25 23:31:23 +0300

    refactor : add AssemblyReference  for Vogel.Social.Presentation

---
+++ Vogel.Social.Presentation/AssemblyReference.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+
+namespace Vogel.Social.Presentation
+{
+    public static class AssemblyReference
+    {
+        public static Assembly Assembly = Assembly.GetExecutingAssembly();
+    }
+}
+
