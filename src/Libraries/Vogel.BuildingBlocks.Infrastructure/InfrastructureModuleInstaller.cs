e9f28f9fbcc0f5ffad66bdc92888e3a4808d5f64
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-27 01:49:17 +0300

    refactor : add module installer for Vogel.BuildingBlocks.Infrastructure

---
+++ src/Libraries/Vogel.BuildingBlocks.Infrastructure/InfrastructureModuleInstaller.cs
@@ -0,0 +1,38 @@
+﻿using Minio;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+using Vogel.BuildingBlocks.Infrastructure.S3Storage;
+using Vogel.BuildingBlocks.Infrastructure.Security;
+
+namespace Vogel.BuildingBlocks.Infrastructure
+{
+    public class InfrastructureModuleInstaller : IModuleInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            ConfigureS3StorageProvider(services, configuration);
+
+            ConfigureSecurity(services);
+        }
+
+        private static void ConfigureS3StorageProvider(IServiceCollection services, IConfiguration configuration)
+        {
+            var s3config = new S3ObjectStorageConfiguration();
+
+            configuration.Bind(S3ObjectStorageConfiguration.CONFIG_KEY, s3config);
+
+            services.AddSingleton(s3config);
+
+            services.AddTransient<IMinioClient, MinioClient>();
+
+            services.AddTransient<IS3ObjectStorageService, S3ObjectStorageService>();
+        }
+
+        private static void ConfigureSecurity(IServiceCollection services)
+        {
+
+            services.AddTransient<IApplicationAuthorizationService, ApplicationAuthorizationService>();
+
+            services.AddTransient<ISecurityContext, SecurityContext>();
+        }
+    }
+}
