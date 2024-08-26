3e8a84fbad712a09a969e7006b1f3e98fd43d9aa
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-27 01:48:46 +0300

    refactor : add Vogel.BuildingBlocks.Infrastructure AssemblyReference

---
+++ src/Libraries/Vogel.BuildingBlocks.Infrastructure/AssemblyReference.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+
+namespace Vogel.BuildingBlocks.Infrastructure
+{
+    public static class AssemblyReference
+    {
+        public static Assembly Assembly = Assembly.GetExecutingAssembly();
+    }
+}
+
