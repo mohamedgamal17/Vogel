2526095007ae3d12870468b5cdee00890c926f49
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-27 21:52:49 +0300

    fix : auto create id for picture efcore entity

--- src/Modules/Social/Vogel.Social.Domain/Pictures/Picture.cs
+++ src/Modules/Social/Vogel.Social.Domain/Pictures/Picture.cs
@@ -4,6 +4,10 @@ namespace Vogel.Social.Domain.Pictures
 {
     public class Picture : AuditedAggregateRoot<string>
     {
+        public Picture()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
         public string File { get; set; }
         public string UserId { get; set; }
     }
