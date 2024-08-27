37aaebca787343b2c124d1160407314fb96f853b
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-27 21:56:16 +0300

    refactor : add picture auto mapper profile class

---
+++ src/Modules/Social/Vogel.Social.Application/Pictures/Profiles/PictureProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Vogel.Social.Domain.Pictures;
+using Vogel.Social.MongoEntities.Pictures;
+
+namespace Vogel.Social.Application.Pictures.Profiles
+{
+    internal class PictureProfile  : Profile
+    {
+        public PictureProfile()
+        {
+            CreateMap<Picture, PictureMongoEntity>();
+        }
+    }
+}
