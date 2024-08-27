c022dce3d89358ce658c1c7bb4ff9e3af7af2db0
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-28 01:25:47 +0300

    refactor : add user auto mapper profile class

---
+++ src/Modules/Social/Vogel.Social.Application/Users/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Vogel.Social.Domain.Users;
+using Vogel.Social.MongoEntities.Users;
+
+namespace Vogel.Social.Application.Users.Profiles
+{
+    internal class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserMongoEntity>();
+        }
+    }
+}
