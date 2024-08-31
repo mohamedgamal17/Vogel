01c7fea70d94393bc5992ba7230c638645fb1bd3
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-01 02:36:44 +0300

    tests : add picture assertion extensions method

---
+++ tests/Modules/Social/Vogel.Social.Application.Tests/Extensions/PictureFluentAssertionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Vogel.Social.Application.Pictures.Commands.CreatePicture;
+using Vogel.Social.Domain.Pictures;
+using Vogel.Social.MongoEntities.Pictures;
+using Vogel.Application.Tests.Extensions;
+using Vogel.Social.Shared.Dtos;
+namespace Vogel.Social.Application.Tests.Extensions
+{
+    public static class PictureFluentAssertionExtensions
+    {
+        public static void AssertMedia(this Picture picture, CreatePictureCommand command , string userId)
+        {
+            picture.File.Should().Be(command.Name);
+            picture.UserId.Should().Be(userId);
+        }
+
+        public static void AssertMediaMongoEntity(this Picture picture, PictureMongoEntity mongoEntity)
+        {
+            picture.Id.Should().Be(mongoEntity.Id);
+            picture.UserId.Should().Be(mongoEntity.UserId);
+            picture.File.Should().Be(mongoEntity.File);
+            picture.AssertAuditingProperties(mongoEntity);
+        }
+
+        public static void AssertMediaDto(this PictureDto dto, Picture picture)
+        {
+            dto.Id.Should().Be(picture.Id);
+            dto.UserId.Should().Be(picture.UserId);
+        }
+    }
+}
