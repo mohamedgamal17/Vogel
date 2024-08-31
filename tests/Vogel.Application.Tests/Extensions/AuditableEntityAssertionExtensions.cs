16ce89cdaa3503f588dca357a78c454a60b491d3
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-01 01:43:46 +0300

    tests : add AuditableEntityAssertionExtensions

---
+++ tests/Vogel.Application.Tests/Extensions/AuditableEntityAssertionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Vogel.BuildingBlocks.Domain.Auditing;
+using Vogel.BuildingBlocks.MongoDb;
+
+namespace Vogel.Application.Tests.Extensions
+{
+    public static class AuditableEntityAssertionExtensions
+    {
+        public static void AssertAuditingProperties(this IAuditedEntity left, IMongoAuditing right)
+        {
+            left.CreatorId.Should().Be(right.CreatorId);
+            left.CreationTime.Should().BeCloseTo(right.CreationTime, TimeSpan.FromMilliseconds(1));
+            left.ModificationTime.Should().BeCloseTo(right.ModificationTime, TimeSpan.FromMilliseconds(1));
+            left.ModifierId.Should().Be(right.ModifierId);
+            left.DeletorId.Should().Be(right.DeletorId);
+            if (left.DeletionTime != null)
+            {
+                left.DeletionTime.Should().BeCloseTo(right.DeletionTime!.Value, TimeSpan.FromMilliseconds(1));
+            }
+
+        }
+
+        public static void AssertAuditingProperties(this IMongoAuditing left, IAuditedEntity right)
+        {
+            left.CreatorId.Should().Be(right.CreatorId);
+            left.CreationTime.Should().BeCloseTo(right.CreationTime, TimeSpan.FromMilliseconds(1));
+            left.ModificationTime.Should().BeCloseTo(right.ModificationTime, TimeSpan.FromMilliseconds(1));
+            left.ModifierId.Should().Be(right.ModifierId);
+            left.DeletorId.Should().Be(right.DeletorId);
+            if (left.DeletionTime != null)
+            {
+                left.DeletionTime.Should().BeCloseTo(right.DeletionTime!.Value, TimeSpan.FromMilliseconds(1));
+            }
+
+        }
+
+    }
+}
