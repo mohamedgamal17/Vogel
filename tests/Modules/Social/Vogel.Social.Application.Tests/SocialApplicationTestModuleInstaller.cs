7a6f740b15faa08ae99a2f391809b5cdb8e6d169
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-01 02:04:51 +0300

    tests : add SocialApplicationTestModuleInstaller

---
+++ tests/Modules/Social/Vogel.Social.Application.Tests/SocialApplicationTestModuleInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Vogel.BuildingBlocks.Infrastructure.Extensions;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+using Vogel.Social.Infrastructure;
+namespace Vogel.Social.Application.Tests
+{
+    public class SocialApplicationTestModuleInstaller : IModuleInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.InstallModule<SocialModuleInstaller>(configuration, environment)
+                .InstallModule<SocialApplicationTestModuleInstaller>(configuration, environment);
+        }
+    }
+}
