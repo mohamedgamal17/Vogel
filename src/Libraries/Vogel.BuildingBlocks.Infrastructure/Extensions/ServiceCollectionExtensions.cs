ef42289f404e918db9401f4bab121556fcc13956
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-31 21:26:36 +0300

    refactor : add Replace method into ServiceCollectionExtensions

--- src/Libraries/Vogel.BuildingBlocks.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Libraries/Vogel.BuildingBlocks.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -43,6 +43,24 @@ namespace Vogel.BuildingBlocks.Infrastructure.Extensions
             return services;
         }
 
+        public static IServiceCollection Replace<TService, TImplementaion>(this IServiceCollection services)
+        {
+            return services.Replace(typeof(TService), typeof(TImplementaion));
+        }
+
+        public static IServiceCollection Replace(this IServiceCollection services,Type service, Type implementaion)
+        {
+            var oldService = services.Single(x => x.ServiceType == service);
+
+            services.Remove(oldService);
+
+            var serviceDescriptor = new ServiceDescriptor(service, service, oldService.Lifetime);
+
+            services.Add(serviceDescriptor);
+
+            return services;
+        }
+
         private static void ResolveServicesInstallers(IEnumerable<Type> types , IServiceCollection services,IConfiguration configuration , IHostEnvironment hostEnvironment)
         {
             foreach (var type in types)
