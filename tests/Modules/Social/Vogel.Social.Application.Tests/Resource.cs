deff745c957960591fca2eb03732e481e999c85f
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-01 02:30:15 +0300

    tests : add resources loader utility class

---
+++ tests/Modules/Social/Vogel.Social.Application.Tests/Resource.cs
@@ -0,0 +1,19 @@
+﻿namespace Vogel.Social.Application.Tests
+{
+    public static class Resource
+    {
+
+        public static async Task<Stream> LoadImageAsStream()
+        {
+            using var streamReader = new StreamReader("resources/fake.png");
+
+            var memoryStream = new MemoryStream();
+
+            await streamReader.BaseStream.CopyToAsync(memoryStream);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return memoryStream;
+        }
+    }
+}
