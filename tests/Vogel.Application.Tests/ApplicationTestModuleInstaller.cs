8698b50e87ea8189158ff37f64ce5fbeb6e330af
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-08-31 21:53:50 +0300

    tests : add ApplicationTestModuleInstaller

---
+++ tests/Vogel.Application.Tests/ApplicationTestModuleInstaller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Vogel.Application.Tests.Services;
+using Vogel.BuildingBlocks.Infrastructure;
+using Vogel.BuildingBlocks.Infrastructure.Extensions;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+using Vogel.BuildingBlocks.Infrastructure.S3Storage;
+using Vogel.BuildingBlocks.Infrastructure.Security;
+namespace Vogel.Application.Tests
+{
+    public class ApplicationTestModuleInstaller : IModuleInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.
+                InstallModule<InfrastructureModuleInstaller>(configuration, environment)
+                .Replace<IS3ObjectStorageService, FakeS3ObjectService>()
+                .Replace<ISecurityContext, FakeSecurityContext>()
+                .AddTransient<FakeUserService>();
+
+        }
+
+    }
+}
