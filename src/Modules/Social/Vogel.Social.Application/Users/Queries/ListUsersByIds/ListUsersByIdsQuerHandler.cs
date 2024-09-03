5c5db1ccaf4fb3fdbeb435c65ffcc67bac877c5b
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-04 02:55:27 +0300

    refactor : add list user by ids query

---
+++ src/Modules/Social/Vogel.Social.Application/Users/Queries/ListUsersByIds/ListUsersByIdsQuerHandler.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Driver;
+using Vogel.BuildingBlocks.Application.Requests;
+using Vogel.BuildingBlocks.MongoDb.Extensions;
+using Vogel.BuildingBlocks.Shared.Models;
+using Vogel.BuildingBlocks.Shared.Results;
+using Vogel.Social.Application.Users.Factories;
+using Vogel.Social.MongoEntities.Users;
+using Vogel.Social.Shared.Dtos;
+
+namespace Vogel.Social.Application.Users.Queries.ListUsersByIds
+{
+    public class ListUsersByIdsQuerHandler : IApplicationRequestHandler<ListUsersByIdsQuery, Paging<UserDto>>
+    {
+        private readonly UserMongoRepository _userMongoRepository;
+        private readonly IUserResponseFactory _userResponseFactory;
+
+        public ListUsersByIdsQuerHandler(UserMongoRepository userMongoRepository, IUserResponseFactory userResponseFactory)
+        {
+            _userMongoRepository = userMongoRepository;
+            _userResponseFactory = userResponseFactory;
+        }
+
+        public async Task<Result<Paging<UserDto>>> Handle(ListUsersByIdsQuery request, CancellationToken cancellationToken)
+        {
+            var query = _userMongoRepository.GetUserAsAggregate();
+
+            if (request.Ids != null)
+            {
+                query = query.Match(Builders<UserMongoView>.Filter.In(x => x.Id, request.Ids));
+            }
+
+            var paged = await query.ToPaged(request.Cursor, request.Limit, request.Asending);
+
+            var result = new Paging<UserDto>
+            {
+                Data = await _userResponseFactory.PrepareListUserDto(paged.Data),
+                Info = paged.Info
+            };
+
+            return result;
+        }
+    }
+}
---
+++ src/Modules/Social/Vogel.Social.Application/Users/Queries/ListUsersByIds/ListUsersByIdsQuery.cs
@@ -0,0 +1,11 @@
+﻿using Vogel.BuildingBlocks.Application.Requests;
+using Vogel.BuildingBlocks.Shared.Models;
+using Vogel.Social.Shared.Dtos;
+
+namespace Vogel.Social.Application.Users.Queries.ListUsersByIds
+{
+    public class ListUsersByIdsQuery : PagingParams, IQuery<Paging<UserDto>>
+    {
+        public IEnumerable<string> Ids { get; set; }
+    }
+}
