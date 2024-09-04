aa7f41ac5b1b6917aff3c541382d0f30c194cdd7
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-05 02:30:52 +0300

    refactor : add extension method to Result to prevent failure

---
+++ src/Libraries/Vogel.BuildingBlocks.Shared/Extensions/ResultExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Vogel.BuildingBlocks.Shared.Results;
+
+namespace Vogel.BuildingBlocks.Shared.Extensions
+{
+    public static class ResultExtensions
+    {
+        public static void ThrowIfFailure<T>(this Result<T> result)
+        {
+            if (result.IsFailure)
+            {
+                throw result.Exception!;
+            }          
+        }
+    }
+}
