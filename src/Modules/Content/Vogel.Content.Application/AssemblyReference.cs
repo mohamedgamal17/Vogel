cc02290ca95736a3dfc178127bac09e3827576bd
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-06 17:58:28 +0300

    refactor : add AssemblyReference for Vogel.Social.Application

---
+++ src/Modules/Content/Vogel.Content.Application/AssemblyReference.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+
+namespace Vogel.Content.Application
+{
+    public static class AssemblyReference
+    {
+        public static Assembly Assembly = Assembly.GetExecutingAssembly();
+    }
+}
+
