8ab41a035362164cb1e6fbdd907ce9c9ff718fac
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-06 18:48:21 +0300

    fix : Rename ContentRepository

--- src/Modules/Content/Vogel.Content.Infrastructure/EntityFramework/Repositories/ContentRepository.cs
+++ src/Modules/Content/Vogel.Content.Infrastructure/EntityFramework/Repositories/ContentRepository.cs
@@ -3,10 +3,10 @@ using Vogel.BuildingBlocks.EntityFramework.Repositories;
 using Vogel.Content.Domain;
 namespace Vogel.Content.Infrastructure.EntityFramework.Repositories
 {
-    public class SocialRepository<TEntity> : EFCoreRepository<TEntity, ContentDbContext>, IContentRepository<TEntity>
+    public class ContentRepository<TEntity> : EFCoreRepository<TEntity, ContentDbContext>, IContentRepository<TEntity>
         where TEntity : class, IEntity
     {
-        public SocialRepository(ContentDbContext dbContext) : base(dbContext)
+        public ContentRepository(ContentDbContext dbContext) : base(dbContext)
         {
 
         }
