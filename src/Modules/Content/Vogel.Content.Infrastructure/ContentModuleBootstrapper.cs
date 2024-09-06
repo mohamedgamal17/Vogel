2f02cfb67793dc4e561bb3234e766896fa0743ce
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-06 18:58:24 +0300

    refactor : add content module installer

---
+++ src/Modules/Content/Vogel.Content.Infrastructure/ContentModuleBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+using Vogel.Content.Infrastructure.EntityFramework;
+namespace Vogel.Content.Infrastructure
+{
+    public class ContentModuleBootstrapper : IModuleBootstrapper
+    {
+        public async Task Bootstrap(IServiceProvider serviceProvider)
+        {
+            var dbContext = serviceProvider.GetRequiredService<ContentDbContext>();
+
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
---
+++ src/Modules/Content/Vogel.Content.Infrastructure/ContentModuleInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Vogel.BuildingBlocks.Infrastructure.Extensions;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+
+namespace Vogel.Content.Infrastructure
+{
+    public class ContentModuleInstaller : IModuleInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.InstallServiceFromAssembly(configuration, environment,
+                    Assembly.GetExecutingAssembly()
+                );
+
+            services.AddTransient<IModuleBootstrapper, ContentModuleBootstrapper>();
+        }
+    }
+}
