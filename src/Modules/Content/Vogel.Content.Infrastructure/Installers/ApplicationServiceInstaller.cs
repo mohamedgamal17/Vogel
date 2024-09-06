b7e9cad21bf3caa68cd2f3ac3cc82f2f81f1da28
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-06 18:48:41 +0300

    refacotr : add content module services installers

---
+++ src/Modules/Content/Vogel.Content.Infrastructure/Installers/ApplicationServiceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Vogel.BuildingBlocks.Application.Extensions;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+namespace Vogel.Content.Infrastructure.Installers
+{
+    public class ApplicationServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Application.AssemblyReference.Assembly))
+                .RegisterFactoriesFromAssembly(Application.AssemblyReference.Assembly)
+                .RegisterPoliciesHandlerFromAssembly(Application.AssemblyReference.Assembly)
+                .AddAutoMapper(Application.AssemblyReference.Assembly);
+
+        }
+    }
+}
---
+++ src/Modules/Content/Vogel.Content.Infrastructure/Installers/EntityFrameworkServiceInstaller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Vogel.BuildingBlocks.EntityFramework.Interceptors;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+using Vogel.Content.Domain;
+using Vogel.Content.Infrastructure.EntityFramework;
+using Vogel.Content.Infrastructure.EntityFramework.Repositories;
+namespace Vogel.Content.Infrastructure.Installers
+{
+    public class EntityFrameworkServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.AddDbContext<ContentDbContext>((sp, opt) =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("Default"), cfg => cfg
+                    .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
+                   )
+                .AddInterceptors(
+                        sp.GetRequiredService<AuditableEntityInterceptors>(),
+                        sp.GetRequiredService<DispatchDomainEventInterceptor>()
+                   );
+            });
+
+            services.AddTransient(typeof(IContentRepository<>), typeof(ContentRepository<>));
+        }
+    }
+}
