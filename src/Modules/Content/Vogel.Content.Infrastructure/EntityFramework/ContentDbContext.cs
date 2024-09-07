3e23af79f1680e30b48b04c53d0b3592628fe237
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-08 01:35:49 +0300

    refactor : register efcore entities configuration

--- src/Modules/Content/Vogel.Content.Infrastructure/EntityFramework/ContentDbContext.cs
+++ src/Modules/Content/Vogel.Content.Infrastructure/EntityFramework/ContentDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using Vogel.BuildingBlocks.EntityFramework;
+using Vogel.Content.Infrastructure.EntityFramework.Constants;
 
 namespace Vogel.Content.Infrastructure.EntityFramework
 {
@@ -8,6 +10,15 @@ namespace Vogel.Content.Infrastructure.EntityFramework
         public ContentDbContext(DbContextOptions<ContentDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema(ContentDbConstants.Schema);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
