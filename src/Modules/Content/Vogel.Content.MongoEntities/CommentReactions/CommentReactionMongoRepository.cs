f8e51143c4a663d64fe315aa529648e1091ab153
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-07 20:20:01 +0300

    refactor : add ReactionSummary into CommentReactionMongoRepository

---
+++ src/Modules/Content/Vogel.Content.MongoEntities/CommentReactions/CommentReactionMongoRepository.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+using Vogel.BuildingBlocks.MongoDb;
+using Vogel.BuildingBlocks.MongoDb.Extensions;
+using Vogel.BuildingBlocks.Shared.Models;
+using Vogel.Content.MongoEntities.PostReactions;
+namespace Vogel.Content.MongoEntities.CommentReactions
+{
+    public class CommentReactionMongoRepository : MongoRepository<CommentReactionMongoEntity>
+    {
+        public CommentReactionMongoRepository(IMongoDatabase mongoDatabase) : base(mongoDatabase)
+        {
+
+        }
+
+        public async Task<Paging<CommentReactionSummaryMongoView>> ListCommentsReactionsSummary(IEnumerable<string> ids, string? cursor = null, int limit = 10, bool ascending = false)
+        {
+            var query = AsMongoCollection()
+                .Aggregate()
+                .Match(Filter.In(x => x.CommentId, ids))
+                .Group(k => k.CommentId, grouped => new CommentReactionSummaryMongoView
+                {
+                    Id = grouped.Key,
+                    TotalLike = grouped.Where(x => x.Type == ReactionType.Like).Count(),
+                    TotalLove = grouped.Where(x => x.Type == ReactionType.Love).Count(),
+                    TotalAngry = grouped.Where(x => x.Type == ReactionType.Angry).Count(),
+                    TotalLaugh = grouped.Where(x => x.Type == ReactionType.Laugh).Count(),
+                    TotalSad = grouped.Where(x => x.Type == ReactionType.Sad).Count()
+                });
+
+            var data = await query.ToPaged(cursor, limit, ascending);
+
+            return data;
+        }
+
+        public async Task<CommentReactionSummaryMongoView?> GetCommentReactionSummary(string postId)
+        {
+            var query = AsMongoCollection()
+             .Aggregate()
+             .Match(Filter.Eq(x => x.CommentId, postId))
+             .Group(k => k.CommentId, grouped => new CommentReactionSummaryMongoView
+             {
+                 Id = grouped.Key,
+                 TotalLike = grouped.Where(x => x.Type == ReactionType.Like).Count(),
+                 TotalLove = grouped.Where(x => x.Type == ReactionType.Love).Count(),
+                 TotalAngry = grouped.Where(x => x.Type == ReactionType.Angry).Count(),
+                 TotalLaugh = grouped.Where(x => x.Type == ReactionType.Laugh).Count(),
+                 TotalSad = grouped.Where(x => x.Type == ReactionType.Sad).Count()
+             });
+
+            return await query.SingleOrDefaultAsync();
+        }
+    }
+}
