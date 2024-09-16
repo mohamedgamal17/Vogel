32c00a53e023d58028f9ca7a1f7625cd04e2f8dc
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-17 02:28:47 +0300

    tests : add media initial tests

---
+++ tests/Modules/Content/Vogel.Content.Application.Tests/Medias/CreateMediaCommandHandlerTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Vogel.Application.Tests.Extensions;
+using Vogel.BuildingBlocks.MongoDb;
+using Vogel.Content.Application.Medias.Commands.CreateMedia;
+using Vogel.Content.Application.Tests.Extensions;
+using Vogel.Content.Domain;
+using Vogel.Content.Domain.Medias;
+using Vogel.Content.MongoEntities.Medias;
+namespace Vogel.Content.Application.Tests.Medias
+{
+    public class CreateMediaCommandHandlerTests : ContentTestFixture
+    {
+        public IContentRepository<Media> MediaRepository { get; }
+        public IMongoRepository<MediaMongoEntity> MediaMongoRepository { get; }
+        public CreateMediaCommandHandlerTests()
+        {
+            MediaRepository = ServiceProvider.GetRequiredService<IContentRepository<Media>>();
+            MediaMongoRepository = ServiceProvider.GetRequiredService<IMongoRepository<MediaMongoEntity>>();
+        }
+
+        [Test]
+        public async Task Should_upload_user_media()
+        {
+            UserService.Login();
+
+            var command = new CreateMediaCommand
+            {
+                Content = await Resource.LoadImageAsStream(),
+                MediaType = Domain.Medias.MediaType.Image,
+                MimeType = "image/png",
+                Name = Guid.NewGuid().ToString()
+            };
+
+            var result = await Mediator.Send(command);
+
+            result.ShouldBeSuccess();
+
+            var media = await MediaRepository.FindByIdAsync(result.Value!.Id);
+
+            var mediaMongoEntity = await MediaMongoRepository.FindByIdAsync(media!.Id);
+
+            mediaMongoEntity.Should().NotBeNull();
+
+            media!.AssertMedia(command, UserService.GetCurrentUser()!.Id);
+
+            media.AssertMediaMongoEntity(mediaMongoEntity!);
+
+            result.Value.AssertMediaDto(media);
+        }
+
+
+        [Test]
+        public async Task Should_failure_while_creating_media_when_user_is_not_authorized()
+        {
+            var command = new CreateMediaCommand
+            {
+                Content = await Resource.LoadImageAsStream(),
+                MediaType = Domain.Medias.MediaType.Image,
+                MimeType = "image/png",
+                Name = Guid.NewGuid().ToString()
+            };
+
+            var result = await Mediator.Send(command);
+
+            result.ShoulBeFailure(typeof(UnauthorizedAccessException));
+        }
+
+ 
+    }
+}
---
+++ tests/Modules/Content/Vogel.Content.Application.Tests/Medias/RemoveMediaCommandHandlerTests.cs
@@ -0,0 +1,92 @@
+﻿using Vogel.BuildingBlocks.MongoDb;
+using Vogel.Content.Domain.Medias;
+using Vogel.Content.Domain;
+using Vogel.Content.MongoEntities.Medias;
+using Microsoft.Extensions.DependencyInjection;
+using Vogel.Content.Application.Medias.Commands.RemoveMedia;
+using Vogel.Application.Tests.Extensions;
+using FluentAssertions;
+using Vogel.BuildingBlocks.Domain.Exceptions;
+
+namespace Vogel.Content.Application.Tests.Medias
+{
+    public class RemoveMediaCommandHandlerTests : ContentTestFixture
+    {
+        public IContentRepository<Media> MediaRepository { get; }
+        public IMongoRepository<MediaMongoEntity> MediaMongoRepository { get; }
+
+        public RemoveMediaCommandHandlerTests()
+        {
+            MediaRepository = ServiceProvider.GetRequiredService<IContentRepository<Media>>();
+            MediaMongoRepository = ServiceProvider.GetRequiredService<IMongoRepository<MediaMongoEntity>>();
+        }
+
+        [Test]
+        public async Task Should_remove_media()
+        {
+            UserService.Login();
+
+            string userId = UserService.GetCurrentUser()!.Id;
+
+            var fakeMedia = await CreateMediaAsync(userId);
+
+            var command = new RemoveMediaCommand
+            {
+                Id = fakeMedia.Id
+            };
+
+            var result = await Mediator.Send(command);
+
+            result.ShouldBeSuccess();
+
+            var media = await MediaRepository.FindByIdAsync(fakeMedia.Id);
+
+            media.Should().BeNull();
+
+            var mongoEntity = await MediaMongoRepository.FindByIdAsync(fakeMedia.Id);
+
+            mongoEntity.Should().BeNull();
+        }
+
+        [Test]
+        public async Task Should_failure_while_removing_media_when_user_is_not_authorized()
+        {
+            var media = await CreateMediaAsync(Guid.NewGuid().ToString());
+
+            var command = new RemoveMediaCommand() { Id = media.Id };
+
+            var result = await Mediator.Send(command);
+
+            result.ShoulBeFailure(typeof(UnauthorizedAccessException));
+        }
+
+        [Test]
+        public async Task Should_failure_while_removing_media_when_user_dose_not_own_this_resource()
+        {
+            var media = await CreateMediaAsync(Guid.NewGuid().ToString());
+
+            UserService.Login();
+
+            var command = new RemoveMediaCommand() { Id = media.Id };
+
+            var result = await Mediator.Send(command);
+
+            result.ShoulBeFailure(typeof(ForbiddenAccessException));
+
+        }
+
+        private async Task<Media> CreateMediaAsync(string userId)
+        {
+            var media = new Media()
+            {
+                MediaType = Domain.Medias.MediaType.Image,
+                Size = 56666,
+                File = Guid.NewGuid().ToString(),
+                MimeType = "image/png",
+                UserId = userId
+            };
+
+            return await MediaRepository.InsertAsync(media);
+        }
+    }
+}
