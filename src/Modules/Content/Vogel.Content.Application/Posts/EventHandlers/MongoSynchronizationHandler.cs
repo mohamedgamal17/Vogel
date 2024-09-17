768a6fc3ba3e2a7f00e9e60e52905c6faaed7c54
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-17 04:09:22 +0300

    refactor : add post mongo db synchronization handler

---
+++ src/Modules/Content/Vogel.Content.Application/Posts/EventHandlers/MongoSynchronizationHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Vogel.BuildingBlocks.Domain.Events;
+using Vogel.Content.Domain.Posts;
+using Vogel.Content.MongoEntities.Posts;
+namespace Vogel.Content.Application.Posts.EventHandlers
+{
+    public class MongoSynchronizationHandler :
+        INotificationHandler<EntityCreatedEvent<Post>>,
+        INotificationHandler<EntityUpdatedEvent<Post>>,
+        INotificationHandler<EntityDeletedEvent<Post>>
+    {
+        private readonly IMapper _mapper;
+
+        private readonly PostMongoRepository _postMongoRepository;
+
+        public MongoSynchronizationHandler(IMapper mapper, PostMongoRepository postMongoRepository)
+        {
+            _mapper = mapper;
+            _postMongoRepository = postMongoRepository;
+        }
+
+        public async Task Handle(EntityCreatedEvent<Post> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Post, PostMongoEntity>(notification.Entity);
+
+            await _postMongoRepository.InsertAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityUpdatedEvent<Post> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Post, PostMongoEntity>(notification.Entity);
+
+            await _postMongoRepository.UpdateAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityDeletedEvent<Post> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Post, PostMongoEntity>(notification.Entity);
+
+            await _postMongoRepository.DeleteAsync(mongoEntity);
+        }
+    }
+}
---
+++ src/Modules/Content/Vogel.Content.Application/Posts/Mappings/PostMongoEntityProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Vogel.Content.Domain.Posts;
+using Vogel.Content.MongoEntities.Posts;
+
+namespace Vogel.Content.Application.Posts.Mappings
+{
+    public class PostMongoEntityProfile : Profile
+    {
+        public PostMongoEntityProfile()
+        {
+            CreateMap<Post, PostMongoEntity>();
+        }
+    }
+}
