0e1ac9ec014cb3f22e9538918cc2c7a8352d0314
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-19 01:24:34 +0300

    refactor : add comment reaction mongo db synchronization handler

---
+++ src/Modules/Content/Vogel.Content.Application/CommentReactions/EventHandlers/CommentReactionSynchronizationHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Vogel.BuildingBlocks.Domain.Events;
+using Vogel.BuildingBlocks.MongoDb;
+using Vogel.Content.Domain.Comments;
+using Vogel.Content.MongoEntities.CommentReactions;
+
+
+namespace Vogel.Content.Application.CommentReactions.EventHandlers
+{
+    public class CommentReactionSynchronizationHandler :
+        INotificationHandler<EntityCreatedEvent<CommentReaction>>,
+        INotificationHandler<EntityUpdatedEvent<CommentReaction>>,
+        INotificationHandler<EntityDeletedEvent<CommentReaction>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMongoRepository<CommentReactionMongoEntity> _commentReactionMongoRepository;
+
+        public CommentReactionSynchronizationHandler(IMapper mapper, CommentReactionMongoRepository commentReactionMongoRepository)
+        {
+            _mapper = mapper;
+            _commentReactionMongoRepository = commentReactionMongoRepository;
+        }
+
+        public async Task Handle(EntityCreatedEvent<CommentReaction> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<CommentReaction, CommentReactionMongoEntity>(notification.Entity);
+
+            await _commentReactionMongoRepository.InsertAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityUpdatedEvent<CommentReaction> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<CommentReaction, CommentReactionMongoEntity>(notification.Entity);
+
+            await _commentReactionMongoRepository.UpdateAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityDeletedEvent<CommentReaction> notification, CancellationToken cancellationToken)
+        {
+            await _commentReactionMongoRepository.DeleteAsync(notification.Entity.Id);
+        }
+    }
+}
---
+++ src/Modules/Content/Vogel.Content.Application/CommentReactions/Mappings/CommentReactionMongoEntityProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Vogel.Content.Domain.Comments;
+using Vogel.Content.MongoEntities.CommentReactions;
+namespace Vogel.Application.CommentReactions.Mappings
+{
+    public class CommentReactionMongoEntityProfile : Profile
+    {
+        public CommentReactionMongoEntityProfile()
+        {
+            CreateMap<CommentReaction, CommentReactionMongoEntity>();
+               
+        }
+    }
+}
