c819e19f0db69ddfbe64a5f20cad157d392ecce6
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-20 19:41:18 +0300

    refactor : add friendship reponse factory

---
+++ src/Modules/Social/Vogel.Social.Application/Friendship/Factories/IFriendshipResponseFactory.cs
@@ -0,0 +1,17 @@
+﻿using Vogel.BuildingBlocks.Application.Factories;
+using Vogel.Social.MongoEntities.Friendship;
+using Vogel.Social.Shared.Dtos;
+namespace Vogel.Social.Application.Friendship.Factories
+{
+    public interface IFriendshipResponseFactory : IResponseFactory
+    {
+
+        Task<List<FriendRequestDto>> PrepareListFriendRequestDto(List<FriendRequestMongoView> friendRequests);
+
+        Task<FriendRequestDto> PrepareFriendRequestDto(FriendRequestMongoView friendRequest);
+
+        Task<List<FriendDto>> PrepareListFriendDto(List<FriendMongoView> friends);
+
+        Task<FriendDto> PrepareFriendDto(FriendMongoView friend);
+    }
+}
