c1059e001c55c0834a12e960bb933776489acc42
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-20 20:08:53 +0300

    refactor : add friendship synchronization handler

---
+++ src/Modules/Social/Vogel.Social.Application/Friendship/EventHandlers/FriendRequestSynchronizationHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Vogel.BuildingBlocks.Domain.Events;
+using Vogel.Social.Domain.Friendship;
+using Vogel.Social.MongoEntities.Friendship;
+namespace Vogel.Social.Application.Friendship.EventHandlers
+{
+    public class FriendRequestSynchronizationHandler :
+        INotificationHandler<EntityCreatedEvent<FriendRequest>>,
+        INotificationHandler<EntityUpdatedEvent<FriendRequest>>,
+        INotificationHandler<EntityDeletedEvent<FriendRequest>>
+    {
+
+        private readonly IMapper _mapper;
+
+        private readonly FriendRequestMongoRepository _friendRequestMongoRepository;
+
+        public FriendRequestSynchronizationHandler(IMapper mapper, FriendRequestMongoRepository friendRequestMongoRepository)
+        {
+            _mapper = mapper;
+            _friendRequestMongoRepository = friendRequestMongoRepository;
+        }
+
+        public async Task Handle(EntityCreatedEvent<FriendRequest> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<FriendRequest, FriendRequestMongoEntity>(notification.Entity);
+
+            await _friendRequestMongoRepository.InsertAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityUpdatedEvent<FriendRequest> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<FriendRequest, FriendRequestMongoEntity>(notification.Entity);
+
+            await _friendRequestMongoRepository.UpdateAsync(mongoEntity);
+
+        }
+
+        public async Task Handle(EntityDeletedEvent<FriendRequest> notification, CancellationToken cancellationToken)
+        {
+            await _friendRequestMongoRepository.DeleteAsync(notification.Entity.Id);
+        }
+    }
+}
---
+++ src/Modules/Social/Vogel.Social.Application/Friendship/EventHandlers/FriendSynchronizationHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using Vogel.BuildingBlocks.Domain.Events;
+using Vogel.Social.Domain.Friendship;
+using Vogel.Social.MongoEntities.Friendship;
+namespace Vogel.Social.Application.Friendship.EventHandlers
+{
+    public class FriendSynchronizationHandler :
+        INotificationHandler<EntityCreatedEvent<Friend>>,
+        INotificationHandler<EntityUpdatedEvent<Friend>>,
+        INotificationHandler<EntityDeletedEvent<Friend>>
+    {
+        private readonly IMapper _mapper;
+        private readonly FriendMongoRepository _friendMongoRepository;
+
+        public FriendSynchronizationHandler(IMapper mapper, FriendMongoRepository friendMongoRepository)
+        {
+            _mapper = mapper;
+            _friendMongoRepository = friendMongoRepository;
+        }
+
+        public async Task Handle(EntityCreatedEvent<Friend> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Friend, FriendMongoEntity>(notification.Entity);
+            await _friendMongoRepository.InsertAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityUpdatedEvent<Friend> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Friend, FriendMongoEntity>(notification.Entity);
+
+            await _friendMongoRepository.UpdateAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityDeletedEvent<Friend> notification, CancellationToken cancellationToken)
+        {
+            await _friendMongoRepository.DeleteAsync(notification.Entity.Id);
+        }
+    }
+}
