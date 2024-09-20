608835d57e1558346e6d967ed31a59efc4f48175
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-09-21 02:09:22 +0300

    tests : add friendship assertions

---
+++ tests/Modules/Social/Vogel.Social.Application.Tests/Extensions/FriendAssertionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Vogel.Application.Tests.Extensions;
+using Vogel.Social.Domain.Friendship;
+using Vogel.Social.Domain.Users;
+using Vogel.Social.MongoEntities.Friendship;
+using Vogel.Social.Shared.Dtos;
+
+namespace Vogel.Social.Application.Tests.Extensions
+{
+    public static class FriendAssertionExtensions
+    {
+        public static void AssertFriend(this Friend friend, string sourceId, string targetId)
+        {
+            friend.SourceId.Should().Be(sourceId);
+            friend.TargetId.Should().Be(targetId);
+        }
+
+        public static void AssertFriendMongoEntity(this FriendMongoEntity mongoEntity, Friend friend)
+        {
+            mongoEntity.Id.Should().Be(friend.Id);
+            mongoEntity.SourceId.Should().Be(friend.SourceId);
+            friend.AssertAuditingProperties(mongoEntity);
+        }
+
+        public static void AssertFriendDto(this FriendDto friendDto, Friend friend, User? source = null, User? target = null)
+        {
+            friendDto.Id.Should().Be(friend.Id);
+            friendDto.SourceId.Should().Be(friend.SourceId);
+            friendDto.TargetId.Should().Be(friend.TargetId);
+
+            if (source != null)
+            {
+                friendDto.Source.Should().NotBeNull();
+                friendDto.Source.AssertUserDto(source);
+            }
+
+            if (target != null)
+            {
+                friendDto.Target.Should().NotBeNull();
+                friendDto.Target.AssertUserDto(target);
+            }
+        }
+    }
+}
---
+++ tests/Modules/Social/Vogel.Social.Application.Tests/Extensions/FriendRequestAssertionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Vogel.Application.Tests.Extensions;
+using Vogel.Social.Application.Friendship.Commands.SendFriendRequest;
+using Vogel.Social.Domain.Friendship;
+using Vogel.Social.Domain.Users;
+using Vogel.Social.MongoEntities.Friendship;
+using Vogel.Social.Shared.Common;
+using Vogel.Social.Shared.Dtos;
+namespace Vogel.Social.Application.Tests.Extensions
+{
+    public static class FriendRequestAssertionExtensions
+    {
+        public static void AssertSendFriendRequestCommand(this FriendRequest friendRequest, SendFriendRequestCommand command , string userId )
+        {
+            friendRequest.SenderId.Should().Be(userId);
+            friendRequest.ReciverId.Should().Be(command.ReciverId);
+            friendRequest.State.Should().Be(FriendRequestState.Pending);
+        }
+
+
+
+        public static void AssertFriendRequestMongoEntity(this FriendRequestMongoEntity mongoEntity, FriendRequest friendRequest)
+        {
+            mongoEntity.SenderId.Should().Be(friendRequest.SenderId);
+            mongoEntity.ReciverId.Should().Be(friendRequest.ReciverId);
+            mongoEntity.State.Should().Be(friendRequest.State);
+            friendRequest.AssertAuditingProperties(mongoEntity);
+        }
+
+        public static void AssertFriendRequestDto(this FriendRequestDto friendRequestDto, FriendRequest friendRequest, User? sender = null, User? reciver = null)
+        {
+            friendRequestDto.Id.Should().Be(friendRequest.Id);
+            friendRequestDto.SenderId.Should().Be(friendRequest.SenderId);
+
+            if (sender != null)
+            {
+                friendRequestDto.Sender.Should().NotBeNull();
+                friendRequestDto.Sender.AssertUserDto(sender);
+            }
+
+            if (reciver != null)
+            {
+                friendRequestDto.Reciver.Should().NotBeNull();
+                friendRequestDto.Reciver.AssertUserDto(reciver);
+
+            }
+        }
+    }
+}
