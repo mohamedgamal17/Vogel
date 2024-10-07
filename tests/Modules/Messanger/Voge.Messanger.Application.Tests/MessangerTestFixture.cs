cd58e2e9d5a8d74e476b79b732ba90f98d001ab1
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-08 02:40:06 +0300

    tests : add MessangerTestFixture

---
+++ tests/Modules/Messanger/Voge.Messanger.Application.Tests/MessangerTestFixture.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Vogel.Application.Tests;
+using Vogel.BuildingBlocks.Infrastructure.Extensions;
+namespace Voge.Messanger.Application.Tests
+{
+    public class MessangerTestFixture : TestFixture
+    {
+        protected override Task SetupAsync(IServiceCollection services, IConfiguration configuration, IHostEnvironment hostEnvironment)
+        {
+            services.InstallModule<MessangerApplicationTestModuleInstaller>(configuration, hostEnvironment);
+            return Task.CompletedTask;
+        }
+        protected override async Task InitializeAsync(IServiceProvider services)
+        {
+            await services.RunModulesBootstrapperAsync();
+        }
+        protected override async Task ShutdownAsync(IServiceProvider services)
+        {
+            await DropSqlDb();
+            await DropMongoDb();
+        }
+    }
+}
