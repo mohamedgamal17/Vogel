85e353ecf10c048acef1ab98c74753a7d4e877fa
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-07 21:14:45 +0300

    feat(chat-api) : add Vogel.Social.MongoEntities

---
+++ src/Modules/Messanger/Vogel.Messanger.MongoEntities/Messages/MessageMongoConsts.cs
@@ -0,0 +1,7 @@
+﻿namespace Vogel.Messanger.MongoEntities.Messages
+{
+    public class MessageMongoConsts
+    {
+        public string CollectionName = "messages";
+    }
+}
---
+++ src/Modules/Messanger/Vogel.Messanger.MongoEntities/Messages/MessageMongoEntity.cs
@@ -0,0 +1,12 @@
+﻿using Vogel.BuildingBlocks.MongoDb;
+namespace Vogel.Messanger.MongoEntities.Messages
+{
+
+    public class MessageMongoEntity : FullAuditedMongoEntity<string>
+    {
+        public string Content { get; set; }
+        public string SenderId { get; set; }
+        public string ReciverId { get; set; }
+        public bool IsSeen { get; set; }
+    }
+}
