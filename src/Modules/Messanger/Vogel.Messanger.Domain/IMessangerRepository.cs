e4146fa94d8ac0f5a08eddee08cbc7f76befff03
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-07 21:05:57 +0300

    feat(chat-api) : add messanger initial domain

---
+++ src/Modules/Messanger/Vogel.Messanger.Domain/IMessangerRepository.cs
@@ -0,0 +1,8 @@
+﻿using Vogel.BuildingBlocks.Domain.Repositories;
+
+namespace Vogel.Messanger.Domain
+{
+    internal interface IMessangerRepository<T> : IRepository<T> where T: class 
+    {
+    }
+}
---
+++ src/Modules/Messanger/Vogel.Messanger.Domain/Messages/Message.cs
@@ -0,0 +1,15 @@
+﻿using Vogel.BuildingBlocks.Domain.Auditing;
+namespace Vogel.Messanger.Domain.Messages
+{
+    public class Message : AuditedEntity<string>
+    {
+        public string Content { get; set; }
+        public string SenderId { get; set; }
+        public string ReciverId { get; set; }
+        public bool IsSeen { get; set; }
+        public Message()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
