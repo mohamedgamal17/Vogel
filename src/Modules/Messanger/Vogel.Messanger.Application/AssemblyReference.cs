d66da914af479eb2777e9c01b4d1ac7e321375ff
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-08 02:16:51 +0300

    feat(chat-api) : add Vogel.Social.Application  AssemblyReference class

---
+++ src/Modules/Messanger/Vogel.Messanger.Application/AssemblyReference.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+
+namespace Vogel.Messanger.Application
+{
+    public static class AssemblyReference
+    {
+        public static Assembly Assembly = Assembly.GetExecutingAssembly();
+    }
+}
+
