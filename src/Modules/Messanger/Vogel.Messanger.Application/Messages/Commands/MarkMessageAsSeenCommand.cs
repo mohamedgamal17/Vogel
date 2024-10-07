56e3d319146be3d45d9028f60e5bb381fde4d1fb
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-08 01:10:47 +0300

    feature(chat-api) : add initial messages commands

---
+++ src/Modules/Messanger/Vogel.Messanger.Application/Messages/Commands/MarkMessageAsSeenCommand.cs
@@ -0,0 +1,9 @@
+﻿using Vogel.BuildingBlocks.Application.Requests;
+using Vogel.Messanger.Application.Messages.Dtos;
+namespace Vogel.Messanger.Application.Messages.Commands
+{
+    public class MarkMessageAsSeenCommand : ICommand<MessageDto>
+    {
+
+    }
+}
---
+++ src/Modules/Messanger/Vogel.Messanger.Application/Messages/Commands/SendMessageCommand.cs
@@ -0,0 +1,10 @@
+﻿using Vogel.BuildingBlocks.Application.Requests;
+using Vogel.Messanger.Application.Messages.Dtos;
+namespace Vogel.Messanger.Application.Messages.Commands
+{
+    public class SendMessageCommand : ICommand<MessageDto>
+    {
+        public string Content { get; set; }
+        public string ReciverId { get; set; }
+    }
+}
