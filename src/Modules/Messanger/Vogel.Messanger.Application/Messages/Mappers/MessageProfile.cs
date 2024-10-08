1f3c5b57deb9cdbdacc74b9c4b69a17f73dd19c5
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-08 21:35:57 +0300

    feat(chat-api) : add message auto mapper profile

---
+++ src/Modules/Messanger/Vogel.Messanger.Application/Messages/Mappers/MessageProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Vogel.Messanger.Domain.Messages;
+using Vogel.Messanger.MongoEntities.Messages;
+namespace Vogel.Messanger.Application.Messages.Mappers
+{
+    public class MessageProfile : Profile
+    {
+        public MessageProfile()
+        {
+            CreateMap<Message, MessageMongoEntity>();
+        }
+    }
+}
