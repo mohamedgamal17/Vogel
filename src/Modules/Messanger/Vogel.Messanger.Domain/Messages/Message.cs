5cbe355dbdfcf38ab4db57d47ad95af964f9cbc8
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-08 20:51:33 +0300

    feat(chat-api) : add MarkAsSeen method in Message

--- src/Modules/Messanger/Vogel.Messanger.Domain/Messages/Message.cs
+++ src/Modules/Messanger/Vogel.Messanger.Domain/Messages/Message.cs
@@ -6,10 +6,18 @@ namespace Vogel.Messanger.Domain.Messages
         public string Content { get; set; }
         public string SenderId { get; set; }
         public string ReciverId { get; set; }
-        public bool IsSeen { get; set; }
+        public bool IsSeen { get;private set; }
         public Message()
         {
             Id = Guid.NewGuid().ToString();
+        }
+
+        public void MarkAsSeen()
+        {
+            if (!IsSeen)
+            {
+                IsSeen = true;
+            }
         }
     }
 }
