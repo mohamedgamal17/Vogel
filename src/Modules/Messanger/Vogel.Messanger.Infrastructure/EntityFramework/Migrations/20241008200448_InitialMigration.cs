eec7444a84b59505aa814a1b5e424b2963b8ff38
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-08 23:07:50 +0300

    feat(chat-api) : add messanger module efcore migrations

---
+++ src/Modules/Messanger/Vogel.Messanger.Infrastructure/EntityFramework/Migrations/20241008200448_InitialMigration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vogel.Messanger.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Messanger");
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                schema: "Messanger",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    SenderId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    ReciverId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    IsSeen = table.Column<bool>(type: "bit", nullable: false),
+                    CreatorId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifierId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletorId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_CreatorId",
+                schema: "Messanger",
+                table: "Messages",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_DeletorId",
+                schema: "Messanger",
+                table: "Messages",
+                column: "DeletorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ModifierId",
+                schema: "Messanger",
+                table: "Messages",
+                column: "ModifierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ReciverId",
+                schema: "Messanger",
+                table: "Messages",
+                column: "ReciverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                schema: "Messanger",
+                table: "Messages",
+                column: "SenderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages",
+                schema: "Messanger");
+        }
+    }
+}
