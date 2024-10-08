0bee0f52c56e7f3d48a91ac1335dd799ad053c0d
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-08 21:33:10 +0300

    feat(chat-api) : add message mongo db synchronization handler

---
+++ src/Modules/Messanger/Vogel.Messanger.Application/Messages/EventHandlers/MongoSynchronizationHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+using Vogel.BuildingBlocks.Domain.Events;
+using Vogel.BuildingBlocks.MongoDb;
+using Vogel.Messanger.Domain.Messages;
+using Vogel.Messanger.MongoEntities.Messages;
+
+namespace Vogel.Messanger.Application.Messages.EventHandlers
+{
+    public class MongoSynchronizationHandler : 
+        INotificationHandler<EntityCreatedEvent<Message>>,
+        INotificationHandler<EntityUpdatedEvent<Message>>,
+        INotificationHandler<EntityDeletedEvent<Message>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMongoRepository<MessageMongoEntity> _messageMongoRepository;
+
+        public MongoSynchronizationHandler(IMapper mapper, IMongoRepository<MessageMongoEntity> messageMongoRepository)
+        {
+            _mapper = mapper;
+            _messageMongoRepository = messageMongoRepository;
+        }
+
+        public async Task Handle(EntityCreatedEvent<Message> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Message, MessageMongoEntity>(notification.Entity);
+
+            await _messageMongoRepository.InsertAsync(mongoEntity);
+        }
+
+        public Task Handle(EntityUpdatedEvent<Message> notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Handle(EntityDeletedEvent<Message> notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
