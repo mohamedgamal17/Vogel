86578954cf2debd3f32e657d0d96781140217191
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-08 22:20:20 +0300

    feat(chat-api) : add efcore service installer

---
+++ src/Modules/Messanger/Vogel.Messanger.Infrastructure/EntityFramework/Repositories/MessangerRepository.cs
@@ -0,0 +1,15 @@
+﻿using Vogel.BuildingBlocks.Domain;
+using Vogel.BuildingBlocks.EntityFramework.Repositories;
+using Vogel.Messanger.Domain;
+
+namespace Vogel.Messanger.Infrastructure.EntityFramework.Repositories
+{
+    public class MessangerRepository<TEntity> : EFCoreRepository<TEntity, MessangerDbContext>, IMessangerRepository<TEntity>
+        where TEntity : class, IEntity
+    {
+        public MessangerRepository(MessangerDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ src/Modules/Messanger/Vogel.Messanger.Infrastructure/Installers/EntityFrameworkServiceInstaller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Vogel.BuildingBlocks.EntityFramework.Interceptors;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+using Vogel.Messanger.Domain;
+using Vogel.Messanger.Infrastructure.EntityFramework;
+using Vogel.Messanger.Infrastructure.EntityFramework.Repositories;
+namespace Vogel.Messanger.Infrastructure.Installers
+{
+    public class EntityFrameworkServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.AddDbContext<MessangerDbContext>((sp, opt) =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("Default"), cfg => cfg
+                    .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
+                   )
+                .AddInterceptors(
+                        sp.GetRequiredService<AuditableEntityInterceptors>(),
+                        sp.GetRequiredService<DispatchDomainEventInterceptor>()
+                   );
+            });
+
+            services.AddTransient(typeof(IMessangerRepository<>), typeof(MessangerRepository<>));
+        }
+    }
+}
