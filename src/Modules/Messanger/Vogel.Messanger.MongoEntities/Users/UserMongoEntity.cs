95c2466de01d05ec8d5ad16a49c7b074f38f6605
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-13 21:46:56 +0300

    feat(chat-api) : add user mongo db synchronization model

---
+++ src/Modules/Messanger/Vogel.Messanger.MongoEntities/Users/UserMongoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Vogel.BuildingBlocks.MongoDb;
+using Vogel.Social.Shared.Common;
+
+namespace Vogel.Messanger.MongoEntities.Users
+{
+    public class UserMongoEntity : FullAuditedMongoEntity<string>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public string? AvatarId { get; set; }
+    }
+}
---
+++ src/Modules/Messanger/Vogel.Messanger.MongoEntities/Users/UserMongoRepository.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using Vogel.BuildingBlocks.MongoDb;
+
+namespace Vogel.Messanger.MongoEntities.Users
+{
+    public class UserMongoRepository : MongoRepository<UserMongoEntity>
+    {
+        public UserMongoRepository(IMongoDatabase mongoDatabase) : base(mongoDatabase)
+        {
+
+        }
+    }
+}
