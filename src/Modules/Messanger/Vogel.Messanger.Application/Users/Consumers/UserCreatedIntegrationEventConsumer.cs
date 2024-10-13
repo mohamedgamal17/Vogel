85d2f54f4e0463a70e8493439ce0aaac3b967154
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-13 22:44:50 +0300

    feat(chat-ap) : add inital user integration event consumers

---
+++ src/Modules/Messanger/Vogel.Messanger.Application/Users/Consumers/UserCreatedIntegrationEventConsumer.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Vogel.BuildingBlocks.MongoDb;
+using Vogel.Messanger.MongoEntities.Users;
+using Vogel.Social.Shared.Events;
+
+namespace Vogel.Messanger.Application.Users.Consumers
+{
+    public class UserCreatedIntegrationEventConsumer : IConsumer<UserCreatedIntegrationEvent>
+    {
+        private readonly IMongoRepository<UserMongoEntity> _userMongoRepository;
+
+        public UserCreatedIntegrationEventConsumer(IMongoRepository<UserMongoEntity> userMongoRepository)
+        {
+            _userMongoRepository = userMongoRepository;
+        }
+
+        public async Task Consume(ConsumeContext<UserCreatedIntegrationEvent> context)
+        {
+            var mongoEntity = new UserMongoEntity
+            {
+                Id = context.Message.Id,
+                FirstName = context.Message.FirstName,
+                LastName = context.Message.LastName,
+                AvatarId = context.Message.AvatarId,
+                BirthDate = context.Message.BirthDate,
+                Gender = context.Message.Gender,
+                CreationTime = DateTime.UtcNow,
+                ModificationTime = DateTime.UtcNow
+            };
+
+            await _userMongoRepository.InsertAsync(mongoEntity);
+        }
+    }
+}
---
+++ src/Modules/Messanger/Vogel.Messanger.Application/Users/Consumers/UserUpdatedIntegrationEventConsumer.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Vogel.BuildingBlocks.MongoDb;
+using Vogel.Messanger.MongoEntities.Users;
+using Vogel.Social.Shared.Events;
+
+namespace Vogel.Messanger.Application.Users.Consumers
+{
+    public class UserUpdatedIntegrationEventConsumer : IConsumer<UserUpdatedIntegrationEvent>
+    {
+        private readonly IMongoRepository<UserMongoEntity> _userMongoRepository;
+
+        public UserUpdatedIntegrationEventConsumer(IMongoRepository<UserMongoEntity> userMongoRepository)
+        {
+            _userMongoRepository = userMongoRepository;
+        }
+
+        public async Task Consume(ConsumeContext<UserUpdatedIntegrationEvent> context)
+        {
+            var oldEntity =  await _userMongoRepository.FindByIdAsync(context.Message.Id);
+
+            var mongoEntity = new UserMongoEntity
+            {
+                Id = context.Message.Id,
+                FirstName = context.Message.FirstName,
+                LastName = context.Message.LastName,
+                AvatarId = context.Message.AvatarId,
+                BirthDate = context.Message.BirthDate,
+                Gender = context.Message.Gender,
+                CreationTime = oldEntity!.CreationTime,
+                ModificationTime = DateTime.UtcNow
+            };
+
+            await _userMongoRepository.UpdateAsync(mongoEntity);
+        }
+    }
+}
