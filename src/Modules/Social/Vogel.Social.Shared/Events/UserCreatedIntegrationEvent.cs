c095f34fb7afe06afeaed0d624273e94a26e87d6
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-13 21:50:51 +0300

    feat(chat-api) : add initial user integration events

---
+++ src/Modules/Social/Vogel.Social.Shared/Events/UserCreatedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using Vogel.Social.Shared.Common;
+
+namespace Vogel.Social.Shared.Events
+{
+    public class UserCreatedIntegrationEvent
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public string? AvatarId { get; set; }
+    }
+}
