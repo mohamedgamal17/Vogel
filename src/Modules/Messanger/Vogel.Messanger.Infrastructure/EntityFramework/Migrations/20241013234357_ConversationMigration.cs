bab3d8af40abc21e18c112597ab3ef26f757c8b4
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-14 02:49:48 +0300

    feat(chat-api) : add conversation migration

---
+++ src/Modules/Messanger/Vogel.Messanger.Infrastructure/EntityFramework/Migrations/20241013234357_ConversationMigration.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vogel.Messanger.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConversationMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conversations",
+                schema: "Messanger",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    CreatorId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifierId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletorId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conversations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                schema: "Messanger",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ConversationId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    CreatorId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifierId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletorId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conversations_CreatorId",
+                schema: "Messanger",
+                table: "Conversations",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conversations_DeletorId",
+                schema: "Messanger",
+                table: "Conversations",
+                column: "DeletorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conversations_ModifierId",
+                schema: "Messanger",
+                table: "Conversations",
+                column: "ModifierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_CreatorId",
+                schema: "Messanger",
+                table: "Participants",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_DeletorId",
+                schema: "Messanger",
+                table: "Participants",
+                column: "DeletorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_ModifierId",
+                schema: "Messanger",
+                table: "Participants",
+                column: "ModifierId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conversations",
+                schema: "Messanger");
+
+            migrationBuilder.DropTable(
+                name: "Participants",
+                schema: "Messanger");
+        }
+    }
+}
