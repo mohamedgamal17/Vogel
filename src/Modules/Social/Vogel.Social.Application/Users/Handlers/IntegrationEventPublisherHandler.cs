60763bf40558f15178f154b0a15195c13d26bc00
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-13 22:29:40 +0300

    feat(chat-api) : add user integration event publisher

---
+++ src/Modules/Social/Vogel.Social.Application/Users/Handlers/IntegrationEventPublisherHandler.cs
@@ -0,0 +1,50 @@
+﻿using MassTransit;
+using MediatR;
+using Vogel.BuildingBlocks.Domain.Events;
+using Vogel.Social.Domain.Users;
+using Vogel.Social.Shared.Events;
+
+namespace Vogel.Social.Application.Users.Handlers
+{
+    public class IntegrationEventPublisherHandler :
+        INotificationHandler<EntityCreatedEvent<User>>,
+        INotificationHandler<EntityUpdatedEvent<User>>
+    {
+        private readonly IPublishEndpoint _publishEndPoint;
+
+        public IntegrationEventPublisherHandler(IPublishEndpoint publishEndPoint)
+        {
+            _publishEndPoint = publishEndPoint;
+        }
+
+        public async Task Handle(EntityCreatedEvent<User> notification, CancellationToken cancellationToken)
+        {
+            var @event = new UserCreatedIntegrationEvent
+            {
+                Id = notification.Entity.Id,
+                FirstName = notification.Entity.FirstName,
+                LastName = notification.Entity.LastName,
+                AvatarId = notification.Entity.AvatarId,
+                BirthDate = notification.Entity.BirthDate,
+                Gender = notification.Entity.Gender
+            };
+
+            await _publishEndPoint.Publish(@event);
+        }
+
+        public async Task Handle(EntityUpdatedEvent<User> notification, CancellationToken cancellationToken)
+        {
+            var @event = new UserUpdatedIntegrationEvent
+            {
+                Id = notification.Entity.Id,
+                FirstName = notification.Entity.FirstName,
+                LastName = notification.Entity.LastName,
+                AvatarId = notification.Entity.AvatarId,
+                BirthDate = notification.Entity.BirthDate,
+                Gender = notification.Entity.Gender
+            };
+
+            await _publishEndPoint.Publish(@event);
+        }
+    }
+}
