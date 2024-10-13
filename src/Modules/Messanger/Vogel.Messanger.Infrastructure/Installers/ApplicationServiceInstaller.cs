4aeb55b0824a6a03b3476a84fbce9d6b5e307c67
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-13 22:57:48 +0300

    feat(chat-api) : register masstransit consumers

--- src/Modules/Messanger/Vogel.Messanger.Infrastructure/Installers/ApplicationServiceInstaller.cs
+++ src/Modules/Messanger/Vogel.Messanger.Infrastructure/Installers/ApplicationServiceInstaller.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using MassTransit;
+using MassTransit.Configuration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 using Vogel.BuildingBlocks.Application.Extensions;
 using Vogel.BuildingBlocks.Infrastructure.Modularity;
 namespace Vogel.Messanger.Infrastructure.Installers
@@ -14,6 +17,28 @@ namespace Vogel.Messanger.Infrastructure.Installers
                 .RegisterPoliciesHandlerFromAssembly(Application.AssemblyReference.Assembly)
                 .AddAutoMapper(Application.AssemblyReference.Assembly);
 
+            RegisterMassTransitConsumers(services);
+
+        }
+
+
+        private void RegisterMassTransitConsumers(IServiceCollection services)
+        {
+            var consumerTypes = Application.AssemblyReference.Assembly.GetClassesThatImplementing<IConsumer>();
+
+
+            var registerConsumer = typeof(DependencyInjectionConsumerRegistrationExtensions)
+                .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .Single(m =>
+                    m.Name == "RegisterConsumer" &&
+                    m.GetGenericArguments().Length == 1 &&
+                    m.GetParameters().Length == 1);
+
+
+            foreach (var consumerType in consumerTypes)
+            {
+                registerConsumer.MakeGenericMethod(consumerType).Invoke(services, [services]);
+            }
         }
     }
 }
