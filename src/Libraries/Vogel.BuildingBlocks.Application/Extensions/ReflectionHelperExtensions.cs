1855afdf85702872029e103f5bb89c79e2d45c44
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-13 22:51:05 +0300

    feat(chat-api) : add reflection helper extension methods

---
+++ src/Libraries/Vogel.BuildingBlocks.Application/Extensions/ReflectionHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace Vogel.BuildingBlocks.Application.Extensions
+{
+    public static class ReflectionHelperExtensions
+    {
+        public static List<Type> GetClassesThatImplementing<T>(this Assembly assembly)
+        {
+            return GetClassesThatImplementing(assembly, typeof(T));
+        }
+        public static List<Type> GetClassesThatImplementing(this Assembly assembly,Type implementedInterface)
+        {
+           return assembly.GetTypes()
+                .Where(x => x.IsClass)
+                .Where(x => !x.IsAbstract)
+                .Where(c => implementedInterface.IsAssignableFrom(c))
+                .ToList();
+        }
+
+    }
+}
