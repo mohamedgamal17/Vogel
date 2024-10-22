b65ff7d4fccc62cca9309171c274001f0d42132f
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-22 23:31:27 +0300

    feat(chat-api) : add messanger mongo db service installers

---
+++ src/Modules/Messanger/Vogel.Messanger.Infrastructure/Installers/MongoDbServiceInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+using Vogel.BuildingBlocks.MongoDb.Extensions;
+
+namespace Vogel.Messanger.Infrastructure.Installers
+{
+    public class MongoDbServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.RegisterMongoRepositoriesFromAssembly(MongoEntities.AssemblyReference.Assembly)
+                .RegisterMigrationFromAssembly(MongoEntities.AssemblyReference.Assembly);
+
+        }
+    }
+}
