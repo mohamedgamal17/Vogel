1ba6d39a604eff9ec4b77099911a6c0f0b5db273
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-23 00:56:34 +0300

    feat(chat-api) : add message mongo view class

---
+++ src/Modules/Messanger/Vogel.Messanger.MongoEntities/Messages/MessageMongoView.cs
@@ -0,0 +1,14 @@
+﻿using Vogel.BuildingBlocks.MongoDb;
+using Vogel.Messanger.MongoEntities.Users;
+
+namespace Vogel.Messanger.MongoEntities.Messages
+{
+    public class MessageMongoView : FullAuditedMongoEntity<string>
+    {
+        public string Content { get; set; }
+        public string ConversationId { get; set; }
+        public string SenderId { get; set; }
+        public bool IsSeen { get; set; }
+        public UserMongoEntity User { get; set; }
+    }
+}
