7d7e763041ad6fbdeef653c097de64b535f328db
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-23 23:42:21 +0300

    refactor(chat-api) : send message command handler
    -Retrive conversation and check existance
    -Check if user is participant in current conversation

--- src/Modules/Messanger/Vogel.Messanger.Application/Messages/Commands/SendMessage/SendMessageCommandHandler.cs
+++ src/Modules/Messanger/Vogel.Messanger.Application/Messages/Commands/SendMessage/SendMessageCommandHandler.cs
@@ -1,10 +1,12 @@
 ﻿using Vogel.BuildingBlocks.Application.Requests;
+using Vogel.BuildingBlocks.Domain.Exceptions;
 using Vogel.BuildingBlocks.Infrastructure.Security;
-using Vogel.BuildingBlocks.MongoDb;
 using Vogel.BuildingBlocks.Shared.Results;
+using Vogel.Messanger.Application.Conversations.Policies;
 using Vogel.Messanger.Application.Messages.Dtos;
 using Vogel.Messanger.Application.Messages.Factories;
 using Vogel.Messanger.Domain;
+using Vogel.Messanger.Domain.Conversations;
 using Vogel.Messanger.Domain.Messages;
 using Vogel.Messanger.MongoEntities.Messages;
 namespace Vogel.Messanger.Application.Messages.Commands.SendMessage
@@ -12,30 +14,48 @@ namespace Vogel.Messanger.Application.Messages.Commands.SendMessage
     public class SendMessageCommandHandler : IApplicationRequestHandler<SendMessageCommand, MessageDto>
     {
         private readonly IMessangerRepository<Message> _messageRepository;
-        private readonly IMongoRepository<MessageMongoEntity> _messageMongoRepository;
+        private readonly MessageMongoRepository _messageMongoRepository;
+        private readonly IMessangerRepository<Conversation> _conversationRepository;
         private readonly IMessageResponseFactory _messageResponseFactory;
         private readonly ISecurityContext _securityContext;
+        private readonly IApplicationAuthorizationService _applicationAuthorizationService;
 
-        public SendMessageCommandHandler(IMessangerRepository<Message> messageRepository, IMongoRepository<MessageMongoEntity> messageMongoRepository, IMessageResponseFactory messageResponseFactory, ISecurityContext securityContext)
+        public SendMessageCommandHandler(IMessangerRepository<Message> messageRepository, MessageMongoRepository messageMongoRepository, IMessangerRepository<Conversation> conversationRepository, IMessageResponseFactory messageResponseFactory, ISecurityContext securityContext, IApplicationAuthorizationService applicationAuthorizationService)
         {
             _messageRepository = messageRepository;
             _messageMongoRepository = messageMongoRepository;
+            _conversationRepository = conversationRepository;
             _messageResponseFactory = messageResponseFactory;
             _securityContext = securityContext;
+            _applicationAuthorizationService = applicationAuthorizationService;
         }
 
         public async Task<Result<MessageDto>> Handle(SendMessageCommand request, CancellationToken cancellationToken)
         {
+            var conversation = await _conversationRepository.FindByIdAsync(request.ConversationId);
+
+            if(conversation == null)
+            {
+                return new Result<MessageDto>(new EntityNotFoundException(typeof(Conversation), request.ConversationId));
+            }
+
+            var authorizationResult = await _applicationAuthorizationService.AuthorizeAsync(conversation, ConversationOperationalRequirments.IsParticipant);
+
+            if (authorizationResult.IsFailure)
+            {
+                return new Result<MessageDto>(authorizationResult.Exception!);
+            }
+
             var message = new Message
             {
                 SenderId = _securityContext.User!.Id,
-                ReciverId = request.ReciverId,
+                ConversationId = conversation.Id,
                 Content = request.Content,
             };
 
             await _messageRepository.InsertAsync(message);
 
-            var mongoEntity = await  _messageMongoRepository.FindByIdAsync(message.Id);
+            var mongoEntity = await _messageMongoRepository.GetMessageViewbyId(conversation.Id, message.Id);
 
             return await _messageResponseFactory.PrepareMessageDto(mongoEntity!);
         }
