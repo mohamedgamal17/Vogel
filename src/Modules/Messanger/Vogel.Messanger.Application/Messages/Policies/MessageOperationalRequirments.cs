bf85620428c779573b63b82c6c1131ff0de400f1
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-10-23 23:11:35 +0300

    refactor(chat-api) : remove old message authorization policies

--- src/Modules/Messanger/Vogel.Messanger.Application/Messages/Policies/MessageOperationalRequirments.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Vogel.Messanger.Application.Messages.Policies
-{
-    public class MessageOperationalRequirments
-    {
-        public static IsMessagReciverAuthorizationRequirment IsMessageReciver = new IsMessagReciverAuthorizationRequirment();
-    }
-}
