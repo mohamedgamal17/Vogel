6ef8c37ccd492efcc4441310d216d18cbec28663
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-11-08 17:09:37 +0200

    feat(chat-api): add participant endpoints

---
+++ src/Modules/Messanger/Vogel.Messanger.Presentation/Endpoints/Participants/GetParticipantByIdEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Vogel.BuildingBlocks.Infrastructure.Extensions;
+using Vogel.Messanger.Application.Conversations.Dtos;
+using Vogel.Messanger.Application.Conversations.Queries.GetUserConversationParticipant;
+namespace Vogel.Messanger.Presentation.Endpoints.Participants
+{
+    public class GetParticipantByIdEndpoint : Endpoint<GetUserConversationParticipantQuery>
+    {
+        private readonly IMediator _mediator;
+        public GetParticipantByIdEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override void Configure()
+        {
+            Get("{conversationId}/participants/{participantId}");
+            Description(x => x.Produces(StatusCodes.Status200OK, typeof(ParticipantDto)));
+            Group<ConversationRoutingGroup>();
+        }
+
+        public override async Task HandleAsync(GetUserConversationParticipantQuery req, CancellationToken ct)
+        {
+            var result = await _mediator.Send(req);
+
+            var response = result.ToOk();
+
+            await SendResultAsync(response);
+        }
+    }
+}
---
+++ src/Modules/Messanger/Vogel.Messanger.Presentation/Endpoints/Participants/ListParticipantEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Vogel.BuildingBlocks.Infrastructure.Extensions;
+using Vogel.BuildingBlocks.Shared.Models;
+using Vogel.Messanger.Application.Conversations.Dtos;
+using Vogel.Messanger.Application.Conversations.Queries.ListUserConversationParticipant;
+
+namespace Vogel.Messanger.Presentation.Endpoints.Participants
+{
+    public class ListParticipantEndpoint : Endpoint<ListUserConversationParticipantQuery>
+    {
+        private readonly IMediator _mediator;
+
+        public ListParticipantEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override void Configure()
+        {
+            Get("{conversationId}/participants");
+            Description(x => x.Produces(StatusCodes.Status200OK, typeof(Paging<ParticipantDto>)));
+            Group<ConversationRoutingGroup>();
+        }
+
+        public override async Task HandleAsync(ListUserConversationParticipantQuery req, CancellationToken ct)
+        {
+            var result = await _mediator.Send(req);
+
+            var response = result.ToOk();
+
+            await SendResultAsync(response);
+        }
+    }
+}
