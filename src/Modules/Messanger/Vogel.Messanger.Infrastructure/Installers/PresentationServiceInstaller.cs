7abd98cb059d380bcd4a4fe1780f3da3cef38a25
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-11-10 20:28:16 +0200

    feat(chat-api) : add presentation layer service installer

---
+++ src/Modules/Messanger/Vogel.Messanger.Infrastructure/Installers/PresentationServiceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Vogel.BuildingBlocks.Infrastructure.Modularity;
+namespace Vogel.Messanger.Infrastructure.Installers
+{
+    public class PresentationServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.AddFastEndpoints(opt =>
+            {
+                opt.Assemblies = new Assembly[] { Presentation.AssemblyReference.Assembly };
+            });
+        }
+    }
+}
---
+++ src/Modules/Messanger/Vogel.Messanger.Presentation/AssemblyReference.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+namespace Vogel.Messanger.Presentation
+{
+    public static class AssemblyReference
+    {
+        public static Assembly Assembly = Assembly.GetExecutingAssembly();
+    }
+}
+
