539e8017452530a173c090053cbe674bf5825f35
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-09 01:19:21 +0200

    fix(social-module) : Add Authorization Attribute on ListUsersByIdsQuery

--- src/Modules/Social/Vogel.Social.Application/Users/Queries/ListUsersByIds/ListUsersByIdsQuery.cs
+++ src/Modules/Social/Vogel.Social.Application/Users/Queries/ListUsersByIds/ListUsersByIdsQuery.cs
@@ -1,9 +1,11 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.BuildingBlocks.Shared.Models;
 using Vogel.Social.Shared.Dtos;
 
 namespace Vogel.Social.Application.Users.Queries.ListUsersByIds
 {
+    [Authorize]
     public class ListUsersByIdsQuery : PagingParams, IQuery<Paging<UserDto>>
     {
         public IEnumerable<string> Ids { get; set; }
