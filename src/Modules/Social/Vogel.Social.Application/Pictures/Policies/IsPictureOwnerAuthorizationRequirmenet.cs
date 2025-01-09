6a0774d4920a28ed34a6edef40fe81239842adfb
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-09 02:58:15 +0200

    fix(social-module) : Add PictureOwnerAuthorizationRequirment Handleler for monogo entity

--- src/Modules/Social/Vogel.Social.Application/Pictures/Policies/IsPictureOwnerAuthorizationRequirmenet.cs
+++ src/Modules/Social/Vogel.Social.Application/Pictures/Policies/IsPictureOwnerAuthorizationRequirmenet.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using Vogel.Social.Domain.Pictures;
+using Vogel.Social.MongoEntities.Pictures;
 
 namespace Vogel.Social.Application.Pictures.Policies
 {
@@ -12,6 +13,21 @@ namespace Vogel.Social.Application.Pictures.Policies
     public class IsPictureOwnerAuthorizationRequirmenetHandler : AuthorizationHandler<IsPictureOwnerAuthorizationRequirmenet, Picture>
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsPictureOwnerAuthorizationRequirmenet requirement, Picture resource)
+        {
+            string userId = context.User.Claims.Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+            if (userId == resource.UserId)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class IsMongoPictureOwnerAuthorizationRequirmenetHandler : AuthorizationHandler<IsPictureOwnerAuthorizationRequirmenet, PictureMongoEntity>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsPictureOwnerAuthorizationRequirmenet requirement, PictureMongoEntity resource)
         {
             string userId = context.User.Claims.Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
 
