eb3fd266e556bdad50fcdd9ba955cdf12a6bce47
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-12 14:33:15 +0200

    test : Rename IQuerableExtensions to RandomExtension - Add new functions reponsible to pick random (entities-entity) from IEnumerable

---
+++ tests/Vogel.Application.Tests/Extensions/RandomExtension.cs
@@ -0,0 +1,63 @@
+﻿using Bogus;
+using MassTransit.SqlTransport.Topology;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Vogel.Application.Tests.Extensions
+{
+    public static class RandomExtension
+    {
+        public static async Task<List<TEntity>> PickRandom<TEntity>(this IQueryable<TEntity> query,int count)
+            where TEntity : class
+        {
+            var faker = new Faker();
+
+            int totalCount = await query.CountAsync();
+
+            int skipper = faker.Random.Int(0, totalCount);
+
+            return await query
+                .OrderBy(x => EF.Functions.Random())
+                .Skip(skipper)
+                .Take(count)
+                .ToListAsync();
+        }
+
+        public static async Task<TEntity?> PickRandom<TEntity>(this IQueryable<TEntity> query )
+        {
+            return await query
+                .OrderBy(x => EF.Functions.Random())
+                .FirstOrDefaultAsync();
+        }
+
+        public static List<TEntity> PickRandom<TEntity>(this IEnumerable<TEntity> data, int count)
+        {
+            var faker = new Faker();
+
+            int totalCount =  data.Count();
+
+            int skipper = faker.Random.Int(0, totalCount);
+
+            return  data
+                .OrderBy(x => EF.Functions.Random())
+                .Skip(skipper)
+                .Take(count)
+                .ToList();
+        }
+
+        public static TEntity? PickRandom<TEntity>(this IEnumerable<TEntity> data)
+        {
+            var faker = new Faker();
+
+            int totalCount = data.Count();
+
+            int skipper = faker.Random.Int(0, totalCount);
+
+
+            return data
+                .OrderBy(x => EF.Functions.Random())
+                .FirstOrDefault();
+        }
+    }
+}
