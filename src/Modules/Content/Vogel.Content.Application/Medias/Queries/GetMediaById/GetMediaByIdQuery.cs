47d86f8f2d8b4750f211c5b807c05be23b6c7b1d
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-13 15:59:11 +0200

    fix(content-module) : Add Authorization Attribute on Media Queries

--- src/Modules/Content/Vogel.Content.Application/Medias/Queries/GetMediaById/GetMediaByIdQuery.cs
+++ src/Modules/Content/Vogel.Content.Application/Medias/Queries/GetMediaById/GetMediaByIdQuery.cs
@@ -1,7 +1,9 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.Content.Application.Medias.Dtos;
 namespace Vogel.Content.Application.Medias.Queries.GetMediaById
 {
+    [Authorize]
     public class GetMediaByIdQuery : IQuery<MediaDto> 
     {
         public string MediaId { get; set; }
--- src/Modules/Content/Vogel.Content.Application/Medias/Queries/ListMedia/ListMediaQuery.cs
+++ src/Modules/Content/Vogel.Content.Application/Medias/Queries/ListMedia/ListMediaQuery.cs
@@ -1,8 +1,10 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.BuildingBlocks.Shared.Models;
 using Vogel.Content.Application.Medias.Dtos;
 namespace Vogel.Content.Application.Medias.Queries.ListMedia
 {
+    [Authorize]
     public class ListMediaQuery : PagingParams , IQuery<Paging<MediaDto>>
     {
 
