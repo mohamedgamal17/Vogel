2c24b0f901a02e28333f2fe6042807fe117f920b
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-13 15:58:31 +0200

    fix(content-module) : Add IsMediaOwnerAuthorizationHandler for mongo entity

--- src/Modules/Content/Vogel.Content.Application/Medias/Policies/IsMediaOwnerAuthorizationHandler.cs
+++ src/Modules/Content/Vogel.Content.Application/Medias/Policies/IsMediaOwnerAuthorizationHandler.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using Vogel.Content.Domain.Medias;
+using Vogel.Content.MongoEntities.Medias;
 namespace Vogel.Content.Application.Medias.Policies
 {
     public class IsMediaOwnerAuthorizationHandler : AuthorizationHandler<IsMediaOwnerRequirment, Media>
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsMediaOwnerRequirment requirement, Media resource)
+        {
+            string userId = context.User.Claims.Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+            if (userId == resource.UserId)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class IsMediaMongoEntityOwnerAuthorizationHandler : AuthorizationHandler<IsMediaOwnerRequirment, MediaMongoEntity>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsMediaOwnerRequirment requirement, MediaMongoEntity resource)
         {
             string userId = context.User.Claims.Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
 
