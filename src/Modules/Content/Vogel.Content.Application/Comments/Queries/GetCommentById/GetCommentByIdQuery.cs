990cf327e507523dee67b27b761cf448fd9df6b0
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-14 13:57:52 +0200

    fix(content-module) : Add authorization attrbiute on comment queries

--- src/Modules/Content/Vogel.Content.Application/Comments/Queries/GetCommentById/GetCommentByIdQuery.cs
+++ src/Modules/Content/Vogel.Content.Application/Comments/Queries/GetCommentById/GetCommentByIdQuery.cs
@@ -1,7 +1,9 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.Content.Application.Comments.Dtos;
 namespace Vogel.Content.Application.Comments.Queries.GetCommentById
 {
+    [Authorize]
     public class GetCommentByIdQuery : IQuery<CommentDto>
     {
         public string PostId { get; set; }
--- src/Modules/Content/Vogel.Content.Application/Comments/Queries/ListComments/ListCommentsQuery.cs
+++ src/Modules/Content/Vogel.Content.Application/Comments/Queries/ListComments/ListCommentsQuery.cs
@@ -1,8 +1,10 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.BuildingBlocks.Shared.Models;
 using Vogel.Content.Application.Comments.Dtos;
 namespace Vogel.Content.Application.Comments.Queries.ListComments
 {
+    [Authorize]
     public class ListCommentsQuery : PagingParams , IQuery<Paging<CommentDto>>
     {
         public string PostId { get; set; }
