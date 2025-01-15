0b9837d5e18d00a9c6898420c0bcd164c63c87bc
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-15 17:29:09 +0200

    fix(content-module) : Add authorization attributes to post reaction queries

--- src/Modules/Content/Vogel.Content.Application/PostReactions/Queries/GetPostReactionById/GetPostReactionByIdQuery.cs
+++ src/Modules/Content/Vogel.Content.Application/PostReactions/Queries/GetPostReactionById/GetPostReactionByIdQuery.cs
@@ -1,8 +1,10 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.Content.Application.PostReactions.Dtos;
 
 namespace Vogel.Content.Application.PostReactions.Queries.GetPostReactionById
 {
+    [Authorize]
     public class GetPostReactionByIdQuery : IQuery<PostReactionDto>
     {
         public string PostId { get; set; }
--- src/Modules/Content/Vogel.Content.Application/PostReactions/Queries/ListPostReactions/ListPostReactionsQuery.cs
+++ src/Modules/Content/Vogel.Content.Application/PostReactions/Queries/ListPostReactions/ListPostReactionsQuery.cs
@@ -1,8 +1,10 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.BuildingBlocks.Shared.Models;
 using Vogel.Content.Application.PostReactions.Dtos;
 namespace Vogel.Content.Application.PostReactions.Queries.ListPostReactions
 {
+    [Authorize]
     public class ListPostReactionsQuery : PagingParams , IQuery<Paging<PostReactionDto>>
     {
         public string PostId { get; set; }
