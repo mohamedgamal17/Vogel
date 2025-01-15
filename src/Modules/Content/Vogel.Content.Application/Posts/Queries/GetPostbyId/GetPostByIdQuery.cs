9093d1b26e3ebaa03faa397a043fb84c263c4f55
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-16 01:47:22 +0200

    fix(content-module) : Add authorization attrbitue on Post Queries

--- src/Modules/Content/Vogel.Content.Application/Posts/Queries/GetPostbyId/GetPostByIdQuery.cs
+++ src/Modules/Content/Vogel.Content.Application/Posts/Queries/GetPostbyId/GetPostByIdQuery.cs
@@ -1,8 +1,10 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.Content.Application.Posts.Dtos;
 
 namespace Vogel.Content.Application.Posts.Queries.GetPostbyId
 {
+    [Authorize]
     public class GetPostByIdQuery : IQuery<PostDto>
     {
         public string PostId { get; set; }
--- src/Modules/Content/Vogel.Content.Application/Posts/Queries/ListUserPost/ListUserPostQuery.cs
+++ src/Modules/Content/Vogel.Content.Application/Posts/Queries/ListUserPost/ListUserPostQuery.cs
@@ -1,8 +1,10 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.BuildingBlocks.Shared.Models;
 using Vogel.Content.Application.Posts.Dtos;
 namespace Vogel.Content.Application.Posts.Queries.ListUserPost
 {
+    [Authorize]
     public class ListUserPostQuery : PagingParams , IQuery<Paging<PostDto>>
     {
         public string UserId { get; set; }
