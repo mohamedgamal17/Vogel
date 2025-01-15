dbf70e4aa354999f60e0a5c7a6ec4cd9a4e1c18b
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-15 17:30:04 +0200

    test(content-module) : Add post reaction query handler tests

---
+++ tests/Modules/Content/Vogel.Content.Application.Tests/PostReactions/Queries/GetPostReactionByIdQueryHandlerTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Vogel.Application.Tests.Extensions;
+using Vogel.BuildingBlocks.Domain.Exceptions;
+using Vogel.Content.Application.PostReactions.Queries.GetPostReactionById;
+using Vogel.Content.Application.Posts.Queries.GetPostbyId;
+using Vogel.Content.Application.Tests.Extensions;
+using Vogel.Content.Domain;
+using Vogel.Content.Domain.Posts;
+
+namespace Vogel.Content.Application.Tests.PostReactions.Queries
+{
+    public class GetPostReactionByIdQueryHandlerTests : ContentTestFixture
+    {
+        public IContentRepository<Post> PostRepository { get;  }
+        public IContentRepository<PostReaction> PostReactionRepository { get; }
+
+        public GetPostReactionByIdQueryHandlerTests()
+        {
+            PostRepository = ServiceProvider.GetRequiredService<IContentRepository<Post>>();
+            PostReactionRepository = ServiceProvider.GetRequiredService<IContentRepository<PostReaction>>();
+        }
+
+        [Test]
+        public async Task Should_get_post_reaction_by_id()
+        {
+            var fakePost = await PostRepository.AsQuerable().PickRandom();
+
+            var fakePostReaction = await PostReactionRepository.AsQuerable().Where(x => x.PostId == fakePost!.Id).PickRandom();
+
+            AuthenticationService.Login();
+
+            var query = new GetPostReactionByIdQuery
+            {
+                PostId = fakePost!.Id,
+                PostReactionId = fakePostReaction!.Id
+            };
+
+            var result = await Mediator.Send(query);
+
+            result.ShouldBeSuccess();
+
+            result.Value!.AssertPostReactionDto(fakePostReaction);
+        }
+
+        [Test]
+        public async Task Should_failure_while_getting_post_reaction_by_id_when_post_id_is_not_exist()
+        {
+            AuthenticationService.Login();
+
+            var query = new GetPostReactionByIdQuery
+            {
+                PostId = Guid.NewGuid().ToString(),
+                PostReactionId = Guid.NewGuid().ToString()
+            };
+
+            var result = await Mediator.Send(query);
+
+            result.ShoulBeFailure(typeof(EntityNotFoundException));
+        }
+
+        [Test]
+        public async Task Should_failure_while_getting_post_reaction_by_id_when_post_reaction_is_not_exist()
+        {
+            var fakePost = await PostRepository.AsQuerable().PickRandom();
+
+            AuthenticationService.Login();
+
+            var query = new GetPostReactionByIdQuery
+            {
+                PostId = fakePost!.Id,
+                PostReactionId = Guid.NewGuid().ToString()
+            };
+
+            var result = await Mediator.Send(query);
+
+            result.ShoulBeFailure(typeof(EntityNotFoundException));
+        }
+
+        [Test]
+        public async Task Should_failure_while_getting_post_reaction_by_id_when_user_is_not_authorized()
+        {
+            var query = new GetPostReactionByIdQuery
+            {
+                PostId = Guid.NewGuid().ToString(),
+                PostReactionId = Guid.NewGuid().ToString()
+            };
+
+
+            var result = await Mediator.Send(query);
+
+            result.ShoulBeFailure(typeof(UnauthorizedAccessException));
+        }
+    }
+}
---
+++ tests/Modules/Content/Vogel.Content.Application.Tests/PostReactions/Queries/ListPostReactionsQueryHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Vogel.Application.Tests.Extensions;
+using Vogel.BuildingBlocks.Domain.Exceptions;
+using Vogel.Content.Application.PostReactions.Queries.ListPostReactions;
+using Vogel.Content.Domain;
+using Vogel.Content.Domain.Posts;
+
+namespace Vogel.Content.Application.Tests.PostReactions.Queries
+{
+    public class ListPostReactionsQueryHandlerTests : ContentTestFixture
+    {
+        public IContentRepository<Post> PostRepository { get; }
+
+        public ListPostReactionsQueryHandlerTests()
+        {
+            PostRepository = ServiceProvider.GetRequiredService<IContentRepository<Post>>();
+        }
+
+        [Test]
+        public async Task Should_get_post_all_reactions()
+        {
+            var fakePost = await PostRepository.AsQuerable().PickRandom();
+
+            AuthenticationService.Login();
+
+            var query = new ListPostReactionsQuery
+            {
+                PostId = fakePost!.Id,
+                Limit = 10
+            };
+
+            var result = await Mediator.Send(query);
+
+            result.ShouldBeSuccess();
+
+            result.Value!.Data.All(x => x.PostId == fakePost.Id).Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Should_failure_while_getting_post_reactions_when_post_id_is_not_exist()
+        {
+            AuthenticationService.Login();
+
+            var query = new ListPostReactionsQuery
+            {
+                PostId = Guid.NewGuid().ToString()
+            };
+
+            var result = await Mediator.Send(query);
+
+            result.ShoulBeFailure(typeof(EntityNotFoundException));
+        }
+
+        [Test]
+        public async Task Should_failure_while_getting_post_reactions_when_user_is_not_authorized()
+        {
+            var query = new ListPostReactionsQuery
+            {
+                PostId = Guid.NewGuid().ToString()
+            };
+
+            var result = await Mediator.Send(query);
+
+            result.ShoulBeFailure(typeof(UnauthorizedAccessException));
+        }
+    }
+}
