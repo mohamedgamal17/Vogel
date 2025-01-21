f4cc946e070c0dc70faeecde1beaa358dcb7f471
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-01-21 16:06:18 +0200

    fix(messanger-module) : Add authorization attribute on message queries

--- src/Modules/Messanger/Vogel.Messanger.Application/Messages/Queries/GetUserConversationMessage/GetUserConversationMessageQuery.cs
+++ src/Modules/Messanger/Vogel.Messanger.Application/Messages/Queries/GetUserConversationMessage/GetUserConversationMessageQuery.cs
@@ -1,7 +1,9 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.Messanger.Application.Messages.Dtos;
 namespace Vogel.Messanger.Application.Messages.Queries.GetUserConversationMessage
 {
+    [Authorize]
     public class GetUserConversationMessageQuery : IQuery<MessageDto>
     {
         public string ConversationId { get; set; }
--- src/Modules/Messanger/Vogel.Messanger.Application/Messages/Queries/ListUserConversationMessage/ListUserConversationMessageQuery.cs
+++ src/Modules/Messanger/Vogel.Messanger.Application/Messages/Queries/ListUserConversationMessage/ListUserConversationMessageQuery.cs
@@ -1,9 +1,11 @@
-﻿using Vogel.BuildingBlocks.Application.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using Vogel.BuildingBlocks.Application.Requests;
 using Vogel.BuildingBlocks.Shared.Models;
 using Vogel.Messanger.Application.Messages.Dtos;
 
 namespace Vogel.Messanger.Application.Messages.Queries.ListUserConversationMessage
 {
+    [Authorize]
     public class ListUserConversationMessageQuery : PagingParams, IQuery<Paging<MessageDto>>
     {
         public string ConversationId { get; set; }
