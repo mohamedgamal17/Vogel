51633aa3020e0f989c194eaef469e779d286b8fb
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-28 21:11:34 +0300

    refactor(media): Add MongoEntity SynchronizationHandler  to sync mongo db with medias

---
+++ src/Modules/MediaEngine/Vogel.MediaEngine.Application/Medias/EventHandlers/MongoSynchronizationHandler.cs
@@ -0,0 +1,41 @@
+using AutoMapper;
+using MediatR;
+using Vogel.BuildingBlocks.Domain.Events;
+using Vogel.BuildingBlocks.MongoDb;
+using Vogel.MediaEngine.Domain.Medias;
+using Vogel.MediaEngine.MongoEntities.Medias;
+
+namespace Vogel.MediaEngine.Application.Medias.EventHandlers
+{
+    public class MongoSynchronizationHandler :
+        INotificationHandler<EntityCreatedEvent<Media>>,
+        INotificationHandler<EntityUpdatedEvent<Media>>,
+        INotificationHandler<EntityDeletedEvent<Media>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMongoRepository<MediaMongoEntity> _mediaMongoRepository;
+
+        public MongoSynchronizationHandler(IMapper mapper, IMongoRepository<MediaMongoEntity> mediaMongoRepository)
+        {
+            _mapper = mapper;
+            _mediaMongoRepository = mediaMongoRepository;
+        }
+
+        public async Task Handle(EntityCreatedEvent<Media> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Media, MediaMongoEntity>(notification.Entity);
+            await _mediaMongoRepository.ReplaceOrInsertAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityUpdatedEvent<Media> notification, CancellationToken cancellationToken)
+        {
+            var mongoEntity = _mapper.Map<Media, MediaMongoEntity>(notification.Entity);
+            await _mediaMongoRepository.ReplaceOrInsertAsync(mongoEntity);
+        }
+
+        public async Task Handle(EntityDeletedEvent<Media> notification, CancellationToken cancellationToken)
+        {
+            await _mediaMongoRepository.DeleteAsync(notification.Entity.Id);
+        }
+    }
+}
